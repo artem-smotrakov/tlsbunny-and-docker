9a29201319432f6d61c78fdc7c230708037dfd1a
Artem Smotrakov <artem.smotrakov@gmail.com>
2018-01-19 19:09:51 +0300

    Added key shares

---
+++ src/com/gypsyengineer/tlsbunny/tls13/KeyShare.java
@@ -0,0 +1,124 @@
+package com.gypsyengineer.tlsbunny.tls13;
+
+import com.gypsyengineer.tlsbunny.tls.Entity;
+import com.gypsyengineer.tlsbunny.tls.Vector;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
+public class KeyShare {
+    
+    public static class ClientHello implements Entity {
+        
+        public static final int LENGTH_BYTES = 2;
+        public static final int MAX_EXPECTED_LENGTH = 65535;
+
+        private Vector<KeyShareEntry> client_shares;
+
+        public ClientHello() {
+            this(Vector.wrap(LENGTH_BYTES));
+        }
+
+        public ClientHello(Vector<KeyShareEntry> client_shares) {
+            this.client_shares = client_shares;
+        }
+
+        public void set(KeyShareEntry keyShareEntry) {
+            client_shares = Vector.wrap(MAX_EXPECTED_LENGTH, keyShareEntry);
+        }
+
+        public void set(Vector<KeyShareEntry> client_shares) {
+            this.client_shares = client_shares;
+        }
+
+        public void clear() {
+            client_shares.clear();
+        }
+        
+        @Override
+        public int encodingLength() {
+            return client_shares.encodingLength();
+        }
+
+        @Override
+        public byte[] encoding() throws IOException {
+            return client_shares.encoding();
+        }
+
+        public static ClientHello create(KeyShareEntry... keyShareEntries) {
+            return new ClientHello(
+                    Vector.wrap(LENGTH_BYTES, keyShareEntries));
+        }
+
+        public static ClientHello parse(ByteBuffer buffer) {
+            Vector<KeyShareEntry> client_shares = Vector.parse(
+                    buffer, LENGTH_BYTES, (buf) -> KeyShareEntry.parse(buf));
+
+            return new ClientHello(client_shares);
+        }
+    
+    }
+    
+    public static class ServerHello implements Entity {
+
+        private KeyShareEntry server_share;
+
+        public ServerHello(KeyShareEntry server_share) {
+            this.server_share = server_share;
+        }
+
+        public KeyShareEntry getServerShare() {
+            return server_share;
+        }
+
+        public void setServerShare(KeyShareEntry server_share) {
+            this.server_share = server_share;
+        }
+        
+        @Override
+        public int encodingLength() {
+            return server_share.encodingLength();
+        }
+
+        @Override
+        public byte[] encoding() throws IOException {
+            return server_share.encoding();
+        }
+
+        public static ServerHello parse(ByteBuffer buffer) {
+            return new ServerHello(KeyShareEntry.parse(buffer));
+        }
+
+    }
+    
+    public static class HelloRetryRequest implements Entity {
+
+        private NamedGroup selected_group;
+
+        private HelloRetryRequest(NamedGroup selected_group) {
+            this.selected_group = selected_group;
+        }
+
+        public NamedGroup getSelectedGroup() {
+            return selected_group;
+        }
+
+        public void setSelectedGroup(NamedGroup selected_group) {
+            this.selected_group = selected_group;
+        }
+        
+        @Override
+        public int encodingLength() {
+            return selected_group.encodingLength();
+        }
+
+        @Override
+        public byte[] encoding() throws IOException {
+            return selected_group.encoding();
+        }
+
+        public static HelloRetryRequest parse(ByteBuffer buffer) {
+            return new HelloRetryRequest(NamedGroup.parse(buffer));
+        }
+
+    }
+}
---
+++ src/com/gypsyengineer/tlsbunny/tls13/KeyShareEntry.java
@@ -0,0 +1,57 @@
+package com.gypsyengineer.tlsbunny.tls13;
+
+import com.gypsyengineer.tlsbunny.tls.Vector;
+import com.gypsyengineer.tlsbunny.tls.Entity;
+import com.gypsyengineer.tlsbunny.utils.Utils;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
+public class KeyShareEntry implements Entity {
+
+    public static final int KEY_EXCHANGE_LENGTH_BYTES = 2;
+    public static final int MIN_EXPECTED_KEY_EXCHANGE_BYTES = 1;
+    public static final int MAX_EXPECTED_KEY_EXCHANGE_BYTES = 65535;
+
+    private NamedGroup group;
+    private Vector<Byte> key_exchange;
+
+    private KeyShareEntry(NamedGroup group, Vector<Byte> key_exchange) {
+        this.group = group;
+        this.key_exchange = key_exchange;
+    }
+
+    public NamedGroup getNamedGroup() {
+        return group;
+    }
+
+    public void setNamedGroup(NamedGroup group) {
+        this.group = group;
+    }
+
+    public Entity getKeyExchange() {
+        return key_exchange;
+    }
+
+    public void setKeyExchange(Vector<Byte> key_exchange) {
+        this.key_exchange = key_exchange;
+    }
+
+    @Override
+    public int encodingLength() {
+        return Utils.getEncodingLength(group, key_exchange);
+    }
+
+    @Override
+    public byte[] encoding() throws IOException {
+        return Utils.encoding(group, key_exchange);
+    }
+
+    public static KeyShareEntry parse(ByteBuffer buffer) {
+        NamedGroup group = NamedGroup.parse(buffer);
+        Vector<Byte> key_exchange = Vector.parseOpaqueVector(
+                buffer, KEY_EXCHANGE_LENGTH_BYTES);
+        
+        return new KeyShareEntry(group, key_exchange);
+    }
+
+}
