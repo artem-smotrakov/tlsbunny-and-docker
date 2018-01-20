296a08cfd33edeecdb857cf0be77ebb934f4d951
Artem Smotrakov <artem.smotrakov@gmail.com>
2018-01-20 22:07:24 +0300

    Finished Finished message

---
+++ src/com/gypsyengineer/tlsbunny/tls13/Finished.java
@@ -0,0 +1,47 @@
+package com.gypsyengineer.tlsbunny.tls13;
+
+import com.gypsyengineer.tlsbunny.tls.Bytes;
+import java.nio.ByteBuffer;
+
+public class Finished implements HandshakeMessage {
+
+    private Bytes verify_data;
+
+    private Finished(Bytes verify_data) {
+        this.verify_data = verify_data;
+    }
+
+    public byte[] getVerifyData() {
+        return verify_data.encoding();
+    }
+
+    public void setVerifyData(byte[] verify_data) {
+        this.verify_data = new Bytes(verify_data);
+    }
+
+    public void setVerifyData(Bytes verify_data) {
+        this.verify_data = verify_data;
+    }
+
+    @Override
+    public int encodingLength() {
+        return verify_data.encodingLength();
+    }
+
+    @Override
+    public byte[] encoding() {
+        return verify_data.encoding();
+    }
+
+    @Override
+    public HandshakeType type() {
+        return HandshakeType.FINISHED;
+    }
+
+    public static Finished parse(ByteBuffer buffer, int hashLen) {
+        byte[] verify_data = new byte[hashLen];
+        buffer.get(verify_data);
+        return new Finished(new Bytes(verify_data));
+    }
+
+}
