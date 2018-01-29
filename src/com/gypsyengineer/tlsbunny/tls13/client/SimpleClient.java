b39e5f6f88c4aec156c0d3ece1bd72ccc05cde11
Artem Smotrakov <artem.smotrakov@gmail.com>
2018-01-29 20:03:55 +0300

    Added SimpleClient

---
+++ src/com/gypsyengineer/tlsbunny/tls13/client/SimpleClient.java
@@ -0,0 +1,35 @@
+package com.gypsyengineer.tlsbunny.tls13.client;
+
+import com.gypsyengineer.tlsbunny.tls13.crypto.ApplicationDataConnection;
+import com.gypsyengineer.tlsbunny.tls13.handshake.ClientHandshaker;
+import com.gypsyengineer.tlsbunny.tls13.handshake.ECDHENegotiator;
+import com.gypsyengineer.tlsbunny.tls13.struct.CipherSuite;
+import com.gypsyengineer.tlsbunny.tls13.struct.NamedGroup;
+import com.gypsyengineer.tlsbunny.tls13.struct.SignatureScheme;
+import com.gypsyengineer.tlsbunny.utils.CertificateHolder;
+import com.gypsyengineer.tlsbunny.utils.Connection;
+
+public class SimpleClient {
+
+    public static void main(String[] args) throws Exception {
+        ClientHandshaker handshaker = ClientHandshaker.create(
+                SignatureScheme.ecdsa_secp256r1_sha256, 
+                NamedGroup.secp256r1, 
+                ECDHENegotiator.create(NamedGroup.secp256r1), 
+                CipherSuite.TLS_AES_128_GCM_SHA256, 
+                CertificateHolder.NO_CERTIFICATE);
+        
+        try (Connection connection = Connection.create(
+                System.getProperty("tlsbunny.host", "localhost"), 
+                Integer.getInteger("tlsbunny.port", 443))) {
+            
+            handshaker.run(connection);
+            ApplicationDataConnection appDataConnection = 
+                    new ApplicationDataConnection(
+                            handshaker.applicationData(), connection);
+            
+            appDataConnection.write("GET / HTTP/1.1\n\n".getBytes());
+            System.out.println(new String(appDataConnection.read()));
+        }
+    }
+}
