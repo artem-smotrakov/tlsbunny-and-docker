157873e89d4ca5512bc576ebc212cff6bf320b1d
Artem Smotrakov <artem.smotrakov@gmail.com>
2018-02-17 21:36:29 +0100

    Delete scripts/start_openssl_server.sh

--- scripts/start_openssl_server.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-OPENSSL=${OPENSSL:-"openssl"}
-
-${OPENSSL} s_server -key certs/server_key.pem -cert certs/server_cert.der -certform der -accept 10101 -www -tls1_3 -debug -tlsextdebug
