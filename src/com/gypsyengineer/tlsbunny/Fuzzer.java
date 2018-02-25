aeae5f1ea444be96171bd8c3c88a55aa53409691
Artem Smotrakov <artem.smotrakov@gmail.com>
2018-02-25 16:03:42 +0100

    Added a new entry point com.gypsyengineer.tlsbunny.Fuzzer

---
+++ src/com/gypsyengineer/tlsbunny/Fuzzer.java
@@ -0,0 +1,102 @@
+package com.gypsyengineer.tlsbunny;
+
+import com.gypsyengineer.tlsbunny.tls13.client.MutatedClient;
+import com.gypsyengineer.tlsbunny.utils.Config;
+import com.gypsyengineer.tlsbunny.utils.Utils;
+import static com.gypsyengineer.tlsbunny.utils.Utils.achtung;
+import static com.gypsyengineer.tlsbunny.utils.Utils.info;
+import static com.gypsyengineer.tlsbunny.utils.Utils.printf;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Main entry point.
+ */
+public class Fuzzer {
+
+    private static void help() {
+        printf("Available parameters (via system properties):%n");
+        printf("    %s%n", Config.helpHost());
+        printf("    %s%n", Config.helpPort());
+        printf("    %s%n", Config.helpStartTest());
+        printf("    %s%n", Config.helpTotal());
+        printf("    %s%n", Config.helpParts());
+        printf("    %s%n", Config.helpRatios());
+        printf("    %s%n", Config.helpTargets());
+        printf("    %s%n", Config.helpThreads());
+
+        // TODO: need to implement setting targets in command line
+        // printf("Available targets:%n    %s%n",
+        //        String.join(", ", MutatedStructFactory.getAvailableTargets()));
+    }
+
+    private static boolean needHelp(String[] args) {
+        return args.length > 0 && Utils.contains(args[0], "help", "-help", "--help");
+    }
+
+    public static void main(String[] args) throws Exception {
+        if (needHelp(args)) {
+            help();
+            return;
+        }
+
+        // TODO: need to implement setting targets in command line
+        //String[] availableTargets = MutatedStructFactory.getAvailableTargets();
+        //info("fuzzer has the following targets:%n    %s",
+        //        String.join(", ", availableTargets));
+
+        String host = Config.Instance.getHost();
+        int port = Config.Instance.getPort();
+        info("okay, we're going to fuzz %s:%d", host, port);
+
+        // TODO: it would be nice if we could configure fuzzers in command line
+        //       without writing a config file
+        //       to do that, we need to specify targets in command line
+        // String[] targets = Config.getTargets();
+
+        int total = Config.Instance.getTotal();
+        int threads = Config.Instance.getThreads();
+
+        // TODO: config file should contain a common mode like "mode: bit_flip"
+
+        ExecutorService executor = Executors.newFixedThreadPool(threads);
+        try {
+            for (Config.FuzzerConfig fuzzerConfig : Config.Instance.getFuzzerConfigs()) {
+                switch (fuzzerConfig.getFuzzer()) {
+                    case "MutatedClient":
+                        int testsNumber = total / threads;
+                        int startTest = Config.Instance.getStartTest();
+                        while (startTest < testsNumber * threads) {
+                            info("start fuzzer: %s", fuzzerConfig.getFuzzer());
+                            info("      target: %s", fuzzerConfig.getTarget());
+                            info("  first test: %d", startTest);
+                            info(" tatal tests: %d", testsNumber);
+                            MutatedClient.runFuzzer(executor, host, port,
+                                    fuzzerConfig.getTarget(),
+                                    fuzzerConfig.getMode(),
+                                    startTest,
+                                    testsNumber);
+                            startTest += testsNumber;
+                        }
+
+                        MutatedClient.runFuzzer(executor, host, port,
+                                fuzzerConfig.getTarget(),
+                                fuzzerConfig.getMode(),
+                                startTest,
+                                total % threads);
+                        break;
+                    default:
+                        achtung("Unknown fuzzer: %s", fuzzerConfig.getFuzzer());
+                }
+            }
+        } finally {
+            executor.shutdown();
+        }
+
+        // we are so patient ...
+        executor.awaitTermination(365, TimeUnit.DAYS);
+
+        info("phew, we are done!");
+    }
+}
--- src/com/gypsyengineer/tlsbunny/tls13/client/MutatedClient.java
+++ src/com/gypsyengineer/tlsbunny/tls13/client/MutatedClient.java
@@ -12,19 +12,11 @@ import com.gypsyengineer.tlsbunny.utils.CertificateHolder;
 import com.gypsyengineer.tlsbunny.utils.Config;
 import com.gypsyengineer.tlsbunny.utils.Connection;
 import com.gypsyengineer.tlsbunny.utils.Output;
-import com.gypsyengineer.tlsbunny.utils.Utils;
-import static com.gypsyengineer.tlsbunny.utils.Utils.achtung;
-import static com.gypsyengineer.tlsbunny.utils.Utils.info;
-import static com.gypsyengineer.tlsbunny.utils.Utils.printf;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 
 public class MutatedClient implements Runnable {
 
-    public static final int ONE_TEST = 1;
-    public static final int DEFAULT_TESTS_NUMBER = 10000;
-    public static final byte[] HTTP_GET_REQUEST = "GET / HTTP/1.1\n\n".getBytes();
+    private static final byte[] HTTP_GET_REQUEST = "GET / HTTP/1.1\n\n".getBytes();
 
     private final MutatedStructFactory fuzzer;
     private final Output output;
@@ -91,85 +83,7 @@ public class MutatedClient implements Runnable {
         }
     }
 
-    private static void help() {
-        printf("Available parameters (via system properties):%n");
-        printf("    %s%n", Config.helpHost());
-        printf("    %s%n", Config.helpPort());
-        printf("    %s%n", Config.helpStartTest());
-        printf("    %s%n", Config.helpTotal());
-        printf("    %s%n", Config.helpParts());
-        printf("    %s%n", Config.helpRatios());
-        printf("    %s%n", Config.helpTargets());
-        printf("    %s%n", Config.helpThreads());
-        printf("Available targets:%n    %s%n",
-                String.join(", ", MutatedStructFactory.getAvailableTargets()));
-    }
-
-    private static boolean needHelp(String[] args) {
-        return args.length > 0 && Utils.contains(args[0], "help", "-help", "--help");
-    }
-
-    public static void main(String[] args) throws Exception {
-        if (needHelp(args)) {
-            help();
-            return;
-        }
-
-        String[] availableTargets = MutatedStructFactory.getAvailableTargets();
-        info("fuzzer has the following targets:%n    %s",
-                String.join(", ", availableTargets));
-
-        String host = Config.Instance.getHost();
-        int port = Config.Instance.getPort();
-        info("okay, we're going to fuzz %s:%d", host, port);
-
-        // TODO: it would be nice if we could configure fuzzers in command line
-        //       without writing a config file
-        //       to do that, we need to specify targets in command line
-        // String[] targets = Config.getTargets();
-
-        int total = Config.Instance.getTotal();
-        int threads = Config.Instance.getThreads();
-
-        // TODO: config file should contain a common mode like "mode: bit_flip"
-
-        ExecutorService executor = Executors.newFixedThreadPool(threads);
-        try {
-            for (Config.FuzzerConfig fuzzerConfig : Config.Instance.getFuzzerConfigs()) {
-                switch (fuzzerConfig.getFuzzer()) {
-                    case "MutatedClient":
-                        info("fuzzer: %s", fuzzerConfig.getFuzzer());
-                        info("target: %s", fuzzerConfig.getTarget());
-                        int testsNumber = total / threads;
-                        int startTest = Config.Instance.getStartTest();
-                        while (startTest < testsNumber * threads) {
-                            runFuzzer(executor, host, port,
-                                    fuzzerConfig.getTarget(),
-                                    fuzzerConfig.getMode(),
-                                    startTest,
-                                    testsNumber);
-                            startTest += testsNumber;
-                        }
-
-                        runFuzzer(executor, host, port,
-                                fuzzerConfig.getTarget(),
-                                fuzzerConfig.getMode(),
-                                startTest,
-                                total % threads);
-                        break;
-                    default:
-                        achtung("Unknown fuzzer: %s", fuzzerConfig.getFuzzer());
-                }
-            }
-        } finally {
-            executor.shutdown();
-        }
-
-        executor.awaitTermination(365, TimeUnit.DAYS);
-        info("phew, we are done!");
-    }
-
-    private static void runFuzzer(ExecutorService executor,
+    public static void runFuzzer(ExecutorService executor,
             String host, int port,
             String target, String mode,
             int startTest, int testsNumber) {
