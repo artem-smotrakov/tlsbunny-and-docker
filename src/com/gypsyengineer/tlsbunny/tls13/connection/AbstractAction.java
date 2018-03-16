15c304b7f67acbc2fd8958befa34d08f52055fc5
Artem Smotrakov <artem.smotrakov@zalando.de>
2018-03-16 23:12:31 +0100

    Implemented OutgoingClientHello

--- src/com/gypsyengineer/tlsbunny/tls13/connection/AbstractAction.java
+++ src/com/gypsyengineer/tlsbunny/tls13/connection/AbstractAction.java
@@ -1,11 +1,51 @@
 package com.gypsyengineer.tlsbunny.tls13.connection;
 
+import com.gypsyengineer.tlsbunny.tls.Random;
+import com.gypsyengineer.tlsbunny.tls13.handshake.Context;
+import com.gypsyengineer.tlsbunny.tls13.handshake.Negotiator;
+import com.gypsyengineer.tlsbunny.tls13.struct.*;
 import com.gypsyengineer.tlsbunny.utils.Connection;
+
+import java.io.IOException;
 
 public abstract class AbstractAction implements Action {
 
+    static final long DEFAULT_SEED = 0;
+    static final long SEED = Long.getLong("tlsbunny.seed", DEFAULT_SEED);
+
     byte[] data;
     Connection connection;
+    StructFactory factory;
+    boolean succeeded = false;
+    SignatureScheme scheme;
+    NamedGroup group;
+    Negotiator negotiator;
+    Context context;
+
+    @Override
+    public void init(StructFactory factory) {
+        this.factory = factory;
+    }
+
+    @Override
+    public void init(SignatureScheme scheme) {
+        this.scheme = scheme;
+    }
+
+    @Override
+    public void init(NamedGroup group) {
+        this.group = group;
+    }
+
+    @Override
+    public void init(Negotiator negotiator) {
+        this.negotiator = negotiator;
+    }
+
+    @Override
+    public void init(Context context) {
+        this.context = context;
+    }
 
     @Override
     public void init(byte[] data) {
@@ -19,11 +59,47 @@ public abstract class AbstractAction implements Action {
 
     @Override
     public boolean succeeded() {
-        return false;
+        return succeeded;
     }
 
     @Override
     public byte[] data() {
         return data.clone();
+    }
+
+    // helper methods
+
+    Handshake toHandshake(HandshakeMessage message) throws IOException {
+        return factory.createHandshake(message.type(), message.encoding());
+    }
+
+    Extension wrap(SupportedVersions supportedVersions) throws IOException {
+        return factory.createExtension(
+                ExtensionType.supported_versions, supportedVersions.encoding());
+    }
+
+    Extension wrap(SignatureSchemeList signatureSchemeList) throws IOException {
+        return factory.createExtension(
+                ExtensionType.signature_algorithms, signatureSchemeList.encoding());
+    }
+
+    Extension wrap(NamedGroupList namedGroupList) throws IOException {
+        return factory.createExtension(
+                ExtensionType.supported_groups, namedGroupList.encoding());
+    }
+
+    Extension wrap(KeyShare keyShare) throws IOException {
+        return factory.createExtension(
+                ExtensionType.key_share, keyShare.encoding());
+    }
+
+    static Random createRandom() {
+        java.util.Random generator = new java.util.Random(SEED);
+        byte[] random_bytes = new byte[Random.LENGTH];
+        generator.nextBytes(random_bytes);
+        Random random = new Random();
+        random.setBytes(random_bytes);
+
+        return random;
     }
 }
--- src/com/gypsyengineer/tlsbunny/tls13/connection/Action.java
+++ src/com/gypsyengineer/tlsbunny/tls13/connection/Action.java
@@ -1,8 +1,18 @@
 package com.gypsyengineer.tlsbunny.tls13.connection;
 
+import com.gypsyengineer.tlsbunny.tls13.handshake.Context;
+import com.gypsyengineer.tlsbunny.tls13.handshake.Negotiator;
+import com.gypsyengineer.tlsbunny.tls13.struct.NamedGroup;
+import com.gypsyengineer.tlsbunny.tls13.struct.SignatureScheme;
+import com.gypsyengineer.tlsbunny.tls13.struct.StructFactory;
 import com.gypsyengineer.tlsbunny.utils.Connection;
 
 public interface Action {
+    void init(StructFactory factory);
+    void init(SignatureScheme scheme);
+    void init(NamedGroup group);
+    void init(Negotiator negotiator);
+    void init(Context context);
     void init(byte[] data);
     void init(Connection connection);
     void run();
