438d553057e95319e1ee6274a1a7e44d17efa286
Artem Smotrakov <artem.smotrakov@gmail.com>
2018-03-20 22:30:41 +0100

    Receive and send Finished messages in TLSConnection

--- src/com/gypsyengineer/tlsbunny/tls13/connection/IncomingFinished.java
+++ src/com/gypsyengineer/tlsbunny/tls13/connection/IncomingFinished.java
@@ -1,11 +1,81 @@
 package com.gypsyengineer.tlsbunny.tls13.connection;
 
+import com.gypsyengineer.tlsbunny.tls13.crypto.TranscriptHash;
+import com.gypsyengineer.tlsbunny.tls13.struct.Finished;
+import com.gypsyengineer.tlsbunny.tls13.struct.Handshake;
+import com.gypsyengineer.tlsbunny.tls13.struct.TLSInnerPlaintext;
+import com.gypsyengineer.tlsbunny.tls13.struct.TLSPlaintext;
+import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 
 public class IncomingFinished extends AbstractReceivingAction {
 
     @Override
     boolean runImpl(ByteBuffer buffer) throws Exception {
-        return false;
+        TLSPlaintext tlsPlaintext = factory.parser().parseTLSPlaintext(buffer);
+        if (!tlsPlaintext.containsApplicationData()) {
+            throw new IOException("expected a TLSCiphertext");
+        }
+
+        TLSInnerPlaintext tlsInnerPlaintext = factory.parser().parseTLSInnerPlaintext(
+                context.handshakeDecryptor.decrypt(tlsPlaintext.getFragment()));
+
+        if (!tlsInnerPlaintext.containsHandshake()) {
+            throw new IOException("expected a handshake message");
+        }
+
+        Handshake handshake = factory.parser().parseHandshake(
+                tlsInnerPlaintext.getContent());
+
+        if (!handshake.containsFinished()) {
+            throw new IOException("expected a Finished message");
+        }
+
+        return processFinished(handshake);
+    }
+
+    private boolean processFinished(Handshake handshake)
+            throws NoSuchAlgorithmException, IOException, InvalidKeyException {
+
+        Finished message = factory.parser().parseFinished(
+                handshake.getBody(),
+                suite.hashLength());
+
+        byte[] zero_hash_value = new byte[0];
+
+        byte[] verify_key = hkdf.expandLabel(
+                context.server_handshake_traffic_secret,
+                context.finished,
+                zero_hash_value,
+                hkdf.getHashLength());
+
+        byte[] verify_data = hkdf.hmac(
+                verify_key,
+                TranscriptHash.compute(suite.hash(), context.allMessages()));
+
+        boolean success = Arrays.equals(verify_data, message.getVerifyData());
+        if (!success) {
+            throw new RuntimeException();
+        }
+
+        context.setServerFinished(handshake);
+
+        context.client_application_traffic_secret_0 = hkdf.deriveSecret(
+                context.master_secret,
+                context.c_ap_traffic,
+                context.allMessages());
+        context.server_application_traffic_secret_0 = hkdf.deriveSecret(
+                context.master_secret,
+                context.s_ap_traffic,
+                context.allMessages());
+        context.exporter_master_secret = hkdf.deriveSecret(
+                context.master_secret,
+                context.exp_master,
+                context.allMessages());
+
+        return true;
     }
 }
--- src/com/gypsyengineer/tlsbunny/tls13/connection/OutgoingFinished.java
+++ src/com/gypsyengineer/tlsbunny/tls13/connection/OutgoingFinished.java
@@ -1,10 +1,76 @@
 package com.gypsyengineer.tlsbunny.tls13.connection;
 
+import com.gypsyengineer.tlsbunny.tls13.crypto.TranscriptHash;
+import com.gypsyengineer.tlsbunny.tls13.struct.ContentType;
+import com.gypsyengineer.tlsbunny.tls13.struct.Finished;
+import com.gypsyengineer.tlsbunny.tls13.struct.Handshake;
+import com.gypsyengineer.tlsbunny.tls13.struct.ProtocolVersion;
+import com.gypsyengineer.tlsbunny.tls13.struct.TLSInnerPlaintext;
+import static com.gypsyengineer.tlsbunny.tls13.struct.TLSInnerPlaintext.NO_PADDING;
+import com.gypsyengineer.tlsbunny.tls13.struct.TLSPlaintext;
+
 public class OutgoingFinished extends AbstractAction {
 
     @Override
     public Action run() {
+        try {
+            Finished finished = createFinished();
+            Handshake handshake = toHandshake(finished);
+            context.setClientFinished(handshake);
+
+            byte[] zero_hash_value = new byte[0];
+            context.resumption_master_secret = hkdf.deriveSecret(
+                    context.master_secret,
+                    context.res_master,
+                    context.allMessages());
+            context.client_application_write_key = hkdf.expandLabel(
+                    context.client_application_traffic_secret_0,
+                    context.key,
+                    zero_hash_value,
+                    suite.keyLength());
+            context.client_application_write_iv = hkdf.expandLabel(
+                    context.client_application_traffic_secret_0,
+                    context.iv,
+                    zero_hash_value,
+                    suite.ivLength());
+            context.server_application_write_key = hkdf.expandLabel(
+                    context.server_application_traffic_secret_0,
+                    context.key,
+                    zero_hash_value,
+                    suite.keyLength());
+            context.server_application_write_iv = hkdf.expandLabel(
+                    context.server_application_traffic_secret_0,
+                    context.iv,
+                    zero_hash_value,
+                    suite.ivLength());
+
+            connection.send(encrypt(factory.createTLSInnerPlaintext(ContentType.handshake, handshake.encoding(), NO_PADDING)));
+
+            succeeded = true;
+        } catch (Exception e) {
+            succeeded = false;
+        }
+
         return this;
+    }
+
+    private Finished createFinished() throws Exception {
+        byte[] verify_data = hkdf.hmac(
+                context.finished_key,
+                TranscriptHash.compute(suite.hash(), context.allMessages()));
+
+        return factory.createFinished(verify_data);
+    }
+
+    TLSPlaintext[] encrypt(Handshake message) throws Exception {
+        return encrypt(factory.createTLSInnerPlaintext(
+                ContentType.handshake, message.encoding(), NO_PADDING));
+    }
+
+    private TLSPlaintext[] encrypt(TLSInnerPlaintext message) throws Exception {
+        return factory.createTLSPlaintexts(ContentType.application_data,
+                ProtocolVersion.TLSv12,
+                context.handshakeEncryptor.encrypt(message.encoding()));
     }
 
 }
