2c2c9238687b0d642f0a0917b11c6b26e5c2f22d
Artem Smotrakov <artem.smotrakov@gmail.com>
2018-03-24 12:32:42 +0100

    Better encryption in OutgoingFinished

--- src/com/gypsyengineer/tlsbunny/tls13/connection/OutgoingFinished.java
+++ src/com/gypsyengineer/tlsbunny/tls13/connection/OutgoingFinished.java
@@ -1,13 +1,18 @@
 package com.gypsyengineer.tlsbunny.tls13.connection;
 
 import com.gypsyengineer.tlsbunny.tls13.crypto.AEAD;
+import com.gypsyengineer.tlsbunny.tls13.crypto.AesGcm;
 import com.gypsyengineer.tlsbunny.tls13.crypto.TranscriptHash;
+import com.gypsyengineer.tlsbunny.tls13.handshake.Context;
 import com.gypsyengineer.tlsbunny.tls13.struct.ContentType;
 import com.gypsyengineer.tlsbunny.tls13.struct.Finished;
 import com.gypsyengineer.tlsbunny.tls13.struct.Handshake;
 import com.gypsyengineer.tlsbunny.tls13.struct.ProtocolVersion;
 import com.gypsyengineer.tlsbunny.tls13.struct.TLSInnerPlaintext;
+
+import static com.gypsyengineer.tlsbunny.tls13.handshake.Context.ZERO_HASH_VALUE;
 import static com.gypsyengineer.tlsbunny.tls13.struct.TLSInnerPlaintext.NO_PADDING;
+
 import com.gypsyengineer.tlsbunny.tls13.struct.TLSPlaintext;
 
 public class OutgoingFinished extends AbstractAction {
@@ -19,34 +24,32 @@ public class OutgoingFinished extends AbstractAction {
             Handshake handshake = toHandshake(finished);
             context.setClientFinished(handshake);
 
-            byte[] zero_hash_value = new byte[0];
             context.resumption_master_secret = hkdf.deriveSecret(
                     context.master_secret,
-                    context.res_master,
+                    Context.res_master,
                     context.allMessages());
             context.client_application_write_key = hkdf.expandLabel(
                     context.client_application_traffic_secret_0,
-                    context.key,
-                    zero_hash_value,
+                    Context.key,
+                    ZERO_HASH_VALUE,
                     suite.keyLength());
             context.client_application_write_iv = hkdf.expandLabel(
                     context.client_application_traffic_secret_0,
-                    context.iv,
-                    zero_hash_value,
+                    Context.iv,
+                    ZERO_HASH_VALUE,
                     suite.ivLength());
             context.server_application_write_key = hkdf.expandLabel(
                     context.server_application_traffic_secret_0,
-                    context.key,
-                    zero_hash_value,
+                    Context.key,
+                    ZERO_HASH_VALUE,
                     suite.keyLength());
             context.server_application_write_iv = hkdf.expandLabel(
                     context.server_application_traffic_secret_0,
-                    context.iv,
-                    zero_hash_value,
+                    Context.iv,
+                    ZERO_HASH_VALUE,
                     suite.ivLength());
 
-            connection.send(encrypt(factory.createTLSInnerPlaintext(
-                    ContentType.handshake, handshake.encoding(), NO_PADDING)));
+            connection.send(encrypt(handshake));
 
             context.applicationDataEnctyptor = AEAD.createEncryptor(
                     suite.cipher(),
@@ -74,14 +77,23 @@ public class OutgoingFinished extends AbstractAction {
     }
 
     TLSPlaintext[] encrypt(Handshake message) throws Exception {
-        return encrypt(factory.createTLSInnerPlaintext(
-                ContentType.handshake, message.encoding(), NO_PADDING));
+        return factory.createTLSPlaintexts(
+                ContentType.application_data,
+                ProtocolVersion.TLSv12,
+                encrypt(message.encoding()));
     }
 
-    private TLSPlaintext[] encrypt(TLSInnerPlaintext message) throws Exception {
-        return factory.createTLSPlaintexts(ContentType.application_data,
-                ProtocolVersion.TLSv12,
-                context.handshakeEncryptor.update(message.encoding()));
+    private byte[] encrypt(byte[] data) throws Exception {
+        TLSInnerPlaintext tlsInnerPlaintext = factory.createTLSInnerPlaintext(
+                ContentType.handshake, data, NO_PADDING);
+        byte[] plaintext = tlsInnerPlaintext.encoding();
+
+        context.handshakeEncryptor.start();
+        context.handshakeEncryptor.updateAAD(
+                AEAD.getAdditionalData(plaintext.length + AesGcm.TAG_LENGTH_IN_BYTES));
+        context.handshakeEncryptor.update(plaintext);
+
+        return context.handshakeEncryptor.finish();
     }
 
 }
