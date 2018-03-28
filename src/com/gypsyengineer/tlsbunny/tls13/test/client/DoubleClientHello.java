181fe2108ee856ed3e7089ca5a483b3b60eac61f
Artem Smotrakov <artem.smotrakov@gmail.com>
2018-03-28 22:46:28 +0200

    Added DoubleClientHello (take 2)

---
+++ src/com/gypsyengineer/tlsbunny/tls13/test/client/DoubleClientHello.java
@@ -0,0 +1,23 @@
+package com.gypsyengineer.tlsbunny.tls13.test.client;
+
+import com.gypsyengineer.tlsbunny.tls13.connection.*;
+
+public class DoubleClientHello {
+
+    public static void main(String[] args) throws Exception {
+        TLSConnection.init()
+                .target("localhost")
+                .target(10101)
+                .send(new OutgoingClientHello())
+                .expect(new IncomingServerHello())
+                .expect(new IncomingChangeCipherSpec())
+                .expect(new IncomingEncryptedExtensions())
+                .expect(new IncomingCertificate())
+                .expect(new IncomingCertificateVerify())
+                .expect(new IncomingFinished())
+                .produce(new OutgoingFinished())
+                .send(new OutgoingClientHello())
+                .expect(new IncomingAlert())
+                .connect();
+    }
+}
