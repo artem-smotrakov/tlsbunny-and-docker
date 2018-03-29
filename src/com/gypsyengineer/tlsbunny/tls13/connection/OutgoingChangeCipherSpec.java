d0d8be221af660e688e7087c08d89553847ca6ec
Artem Smotrakov <artem.smotrakov@gmail.com>
2018-03-29 22:28:55 +0200

    Added OutgoingChangeCipherSpec

---
+++ src/com/gypsyengineer/tlsbunny/tls13/connection/OutgoingChangeCipherSpec.java
@@ -0,0 +1,26 @@
+package com.gypsyengineer.tlsbunny.tls13.connection;
+
+import com.gypsyengineer.tlsbunny.tls13.struct.*;
+import com.gypsyengineer.tlsbunny.tls13.utils.Helper;
+
+public class OutgoingChangeCipherSpec extends AbstractAction {
+
+    @Override
+    public String name() {
+        return "ChangeCipherSpec";
+    }
+
+    @Override
+    public Action run() throws Exception {
+        ChangeCipherSpec ccs = factory.createChangeCipherSpec(ChangeCipherSpec.VALID_VALUE);
+        TLSPlaintext[] tlsPlaintexts = factory.createTLSPlaintexts(
+                ContentType.change_cipher_spec,
+                ProtocolVersion.TLSv10,
+                ccs.encoding());
+
+        buffer = Helper.store(tlsPlaintexts);
+
+        return this;
+    }
+
+}
