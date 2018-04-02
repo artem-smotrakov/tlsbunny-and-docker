af0e5cbf73e106d33085e261b439e60d530d7082
Artem Smotrakov <artem.smotrakov@gmail.com>
2018-04-02 11:55:20 +0200

    Updated scripts/start_openssl_tls13_server.sh to use OPTIONS for OpenSSL

--- scripts/start_openssl_tls13_server.sh
+++ scripts/start_openssl_tls13_server.sh
@@ -3,4 +3,5 @@
 OPENSSL=${OPENSSL:-"openssl"}
 
 ${OPENSSL} s_server -key certs/server_key.pem -cert certs/server_cert.der \
-	-certform der -accept 10101 -www -tls1_3 -debug -tlsextdebug
+	-certform der -accept 10101 -www -tls1_3 -debug -tlsextdebug \
+	${OPTIONS}
