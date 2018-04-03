3516c36ec4fd7ffcc747dc19cfbf9ede0194f9af
Artem Smotrakov <artem.smotrakov@gmail.com>
2018-04-03 22:40:38 +0200

    Define Analyzer interface

--- src/com/gypsyengineer/tlsbunny/tls13/connection/Analyzer.java
+++ src/com/gypsyengineer/tlsbunny/tls13/connection/Analyzer.java
@@ -1,5 +1,11 @@
 package com.gypsyengineer.tlsbunny.tls13.connection;
 
+import com.gypsyengineer.tlsbunny.tls13.handshake.Context;
+import com.gypsyengineer.tlsbunny.utils.Output;
+
 public interface Analyzer {
 
+    Analyzer set(Output output);
+    Analyzer add(Context context);
+    Analyzer run();
 }
