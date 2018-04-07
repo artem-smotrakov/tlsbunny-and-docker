ca0f49d4c32094072be6690defd02e0a26cc290b
Artem Smotrakov <artem.smotrakov@gmail.com>
2018-04-08 00:57:43 +0200

    Added TLSPlaintextFuzzer

--- src/com/gypsyengineer/tlsbunny/tls13/test/client/FuzzyClient.java
+++ src/com/gypsyengineer/tlsbunny/tls13/test/client/FuzzyClient.java
@@ -8,6 +8,7 @@ public class FuzzyClient {
 
     private static final List<Config> configs = new ArrayList<>();
     static {
+        configs.addAll(Arrays.asList(TLSPlaintextFuzzer.configs));
         configs.addAll(Arrays.asList(ClientHelloFuzzer.configs));
         configs.addAll(Arrays.asList(FinishedFuzzer.configs));
     }
