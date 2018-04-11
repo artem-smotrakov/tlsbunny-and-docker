6a6b332b6cb278eb6db07902b35e25f061c58559
Artem Smotrakov <artem.smotrakov@gmail.com>
2018-04-11 19:04:08 +0200

    Added StartWithCCS

---
+++ src/com/gypsyengineer/tlsbunny/tls13/test/client/StartWithCCS.java
@@ -0,0 +1,18 @@
+package com.gypsyengineer.tlsbunny.tls13.test.client;
+
+import com.gypsyengineer.tlsbunny.tls13.connection.*;
+
+public class StartWithCCS {
+
+    public static void main(String[] args) throws Exception {
+        CommonConfig config = new CommonConfig();
+
+        Engine.init()
+                .target(config.host())
+                .target(config.port())
+                .send(new OutgoingChangeCipherSpec())
+                .send(new OutgoingClientHello())
+                .expect(new IncomingAlert())
+                .connect();
+    }
+}
