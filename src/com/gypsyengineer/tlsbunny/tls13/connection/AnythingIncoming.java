258c18e1290d1e06c99819f16b976e965ac6926d
Artem Smotrakov <artem.smotrakov@gmail.com>
2018-04-11 22:38:51 +0200

    Added AnythingIncoming

---
+++ src/com/gypsyengineer/tlsbunny/tls13/connection/AnythingIncoming.java
@@ -0,0 +1,20 @@
+package com.gypsyengineer.tlsbunny.tls13.connection;
+
+public class AnythingIncoming extends AbstractAction {
+
+    @Override
+    public String name() {
+        return "anything";
+    }
+
+    @Override
+    public Action run() throws Exception {
+        int read = buffer.remaining();
+        buffer.position(buffer.limit());
+        output.info("received %d bytes", read);
+
+        return this;
+    }
+
+    
+}
