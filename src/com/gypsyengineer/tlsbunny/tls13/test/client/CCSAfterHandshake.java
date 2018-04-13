00dd93e976c92e13cf0f2542e9837a58cbf48b22
Artem Smotrakov <artem.smotrakov@gmail.com>
2018-04-13 21:14:59 +0200

    Finished CCSAfterHandshake

--- src/com/gypsyengineer/tlsbunny/tls13/test/client/CCSAfterHandshake.java
+++ src/com/gypsyengineer/tlsbunny/tls13/test/client/CCSAfterHandshake.java
@@ -7,26 +7,20 @@ public class CCSAfterHandshake {
     public static void main(String[] args) throws Exception {
         CommonConfig config = new CommonConfig();
 
-        // TODO: fix it
         Engine.init()
                 .target(config.host())
                 .target(config.port())
-                //.expect(new EncryptedHandshakeAlert())
-                //.expect(new EncryptedApplicationDataAlert())
                 .send(new OutgoingClientHello())
                 .send(new OutgoingChangeCipherSpec())
                 .require(new IncomingServerHello())
-                .require(new IncomingChangeCipherSpec())
                 .require(new IncomingEncryptedExtensions())
                 .require(new IncomingCertificate())
                 .require(new IncomingCertificateVerify())
                 .require(new IncomingFinished())
                 .send(new OutgoingFinished())
                 .allow(new IncomingNewSessionTicket())
-                .send(new OutgoingHttpGetRequest())
-                .require(new IncomingApplicationData())
                 .send(new OutgoingChangeCipherSpec())
-                .require(new AnythingIncoming())
+                .require(new IncomingAlert())
                 .connect()
                 .run(new AlertCheck());
     }
