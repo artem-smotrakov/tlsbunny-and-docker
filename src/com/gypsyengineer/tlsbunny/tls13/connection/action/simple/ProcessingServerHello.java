168f734d61903c4cc96eb07855c4d26761ffd012
Artem Smotrakov <artem.smotrakov@gmail.com>
2018-04-14 14:17:31 +0200

    Added ProcessingServerHello

---
+++ src/com/gypsyengineer/tlsbunny/tls13/connection/action/simple/ProcessingServerHello.java
@@ -0,0 +1,25 @@
+package com.gypsyengineer.tlsbunny.tls13.connection.action.simple;
+
+import com.gypsyengineer.tlsbunny.tls13.connection.action.AbstractAction;
+import com.gypsyengineer.tlsbunny.tls13.connection.action.Action;
+import com.gypsyengineer.tlsbunny.tls13.struct.*;
+
+import java.nio.ByteBuffer;
+
+public class ProcessingServerHello extends AbstractAction {
+
+    @Override
+    public String name() {
+        return "processing a ServerHello";
+    }
+
+    @Override
+    public Action run() throws Exception {
+        ServerHello serverHello = context.factory.parser().parseServerHello(in);
+        out = ByteBuffer.wrap(serverHello.encoding());
+        output.info("received a ServerHello message");
+
+        return this;
+    }
+
+}
