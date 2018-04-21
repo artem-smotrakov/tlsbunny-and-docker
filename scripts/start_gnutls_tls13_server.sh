2f78eb86a020c144384ec7ef957261fb46bd2c3a
Artem Smotrakov <artem.smotrakov@gmail.com>
2018-04-21 11:02:25 +0200

    Specify certificates and options for gnutls-serv

--- scripts/start_gnutls_tls13_server.sh
+++ scripts/start_gnutls_tls13_server.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-gnutls-serv --port=20101 --debug=9999 --http
+gnutls-serv --port=20101 --debug=9999 --http \
+    --x509cafile certs/root_cert.pem \
+    --x509keyfile certs/server_key.pem \
+    --x509certfile certs/server_cert.pem \
+    ${OPTIONS}
