1c331bb2a326574fc2d98bfde279d31735792cc2
Artem Smotrakov <artem.smotrakov@gmail.com>
2018-04-21 14:26:55 +0200

    Added ProcessingCertificate

---
+++ src/com/gypsyengineer/tlsbunny/tls13/connection/action/simple/ProcessingCertificate.java
@@ -0,0 +1,24 @@
+package com.gypsyengineer.tlsbunny.tls13.connection.action.simple;
+
+import com.gypsyengineer.tlsbunny.tls13.connection.action.AbstractAction;
+import com.gypsyengineer.tlsbunny.tls13.connection.action.Action;
+import com.gypsyengineer.tlsbunny.tls13.struct.Certificate;
+import com.gypsyengineer.tlsbunny.tls13.struct.EncryptedExtensions;
+
+public class ProcessingCertificate extends AbstractAction {
+
+    @Override
+    public String name() {
+        return "processing an Certificate";
+    }
+
+    @Override
+    public Action run() throws Exception {
+        Certificate certificate = context.factory.parser().parseCertificate(
+                in, buf -> context.factory.parser().parseX509CertificateEntry(buf));
+        output.info("received a Certificate message");
+
+        return this;
+    }
+
+}
