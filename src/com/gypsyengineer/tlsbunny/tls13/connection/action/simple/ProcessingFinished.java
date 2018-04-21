34c7f7bb7abea3b517f91aded9c74706d951eaea
Artem Smotrakov <artem.smotrakov@gmail.com>
2018-04-21 15:02:29 +0200

    Verify Finished message in ProcessingFinished

--- src/com/gypsyengineer/tlsbunny/tls13/connection/action/simple/ProcessingFinished.java
+++ src/com/gypsyengineer/tlsbunny/tls13/connection/action/simple/ProcessingFinished.java
@@ -2,7 +2,12 @@ package com.gypsyengineer.tlsbunny.tls13.connection.action.simple;
 
 import com.gypsyengineer.tlsbunny.tls13.connection.action.AbstractAction;
 import com.gypsyengineer.tlsbunny.tls13.connection.action.Action;
+import com.gypsyengineer.tlsbunny.tls13.crypto.TranscriptHash;
 import com.gypsyengineer.tlsbunny.tls13.struct.Finished;
+
+import java.util.Arrays;
+
+import static com.gypsyengineer.tlsbunny.tls13.handshake.Context.ZERO_HASH_VALUE;
 
 public class ProcessingFinished extends AbstractAction {
 
@@ -14,7 +19,23 @@ public class ProcessingFinished extends AbstractAction {
     @Override
     public Action run() throws Exception {
         Finished finished = context.factory.parser().parseFinished(in, context.suite.hashLength());
-        output.info("received a Finished message");
+
+        byte[] verify_key = context.hkdf.expandLabel(
+                context.server_handshake_traffic_secret,
+                context.finished,
+                ZERO_HASH_VALUE,
+                context.hkdf.getHashLength());
+
+        byte[] verify_data = context.hkdf.hmac(
+                verify_key,
+                TranscriptHash.compute(context.suite.hash(), context.allMessages()));
+
+        boolean success = Arrays.equals(verify_data, finished.getVerifyData());
+        if (!success) {
+            throw new RuntimeException("verification of Finished failed");
+        }
+
+        output.info("verified Finished message");
 
         return this;
     }
