8c4f49539f4f42fed1be8d51111fb22f5b78c0bf
Artem Smotrakov <artem.smotrakov@gmail.com>
2018-05-06 13:43:38 +0200

    Check keys in ECDHENegotiator

--- src/main/java/com/gypsyengineer/tlsbunny/tls13/handshake/ECDHENegotiator.java
+++ src/main/java/com/gypsyengineer/tlsbunny/tls13/handshake/ECDHENegotiator.java
@@ -8,10 +8,7 @@ import java.io.IOException;
 import java.math.BigInteger;
 import java.security.*;
 import java.security.interfaces.ECPublicKey;
-import java.security.spec.ECGenParameterSpec;
-import java.security.spec.ECPoint;
-import java.security.spec.ECPublicKeySpec;
-import java.security.spec.InvalidKeySpecException;
+import java.security.spec.*;
 import java.util.Arrays;
 import javax.crypto.KeyAgreement;
 
@@ -37,6 +34,12 @@ public class ECDHENegotiator extends AbstractNegotiator {
             keyAgreement.init(kp.getPrivate());
             ECPoint W = ((ECPublicKey) kp.getPublic()).getW();
 
+            // TODO: consider adding a self-check to make sure that the key
+            //       passes checks defined by NIST and Section 5.2.2 of X9.62
+            //       in other words:
+            //
+            //   validate(w);
+
             return factory.createKeyShareEntry(
                     group,
                     createUPR(W).encoding());
@@ -48,13 +51,18 @@ public class ECDHENegotiator extends AbstractNegotiator {
     @Override
     public void processKeyShareEntry(KeyShareEntry entry) throws NegotiatorException {
         if (!group.equals(entry.getNamedGroup())) {
-            throw new IllegalArgumentException();
+            output.achtung("expected group: %s", group);
+            output.achtung("received group: %s", entry.getNamedGroup());
+            throw new NegotiatorException("unexpected group");
         }
 
         try {
+            ECPoint point = convertToECPoint(entry);
+            validate(point);
+
             PublicKey key = KeyFactory.getInstance("EC").generatePublic(
                     new ECPublicKeySpec(
-                            convertToECPoint(entry),
+                            point,
                             secpParameters.ecParameterSpec));
 
             keyAgreement.doPhase(key, true);
@@ -80,8 +88,12 @@ public class ECDHENegotiator extends AbstractNegotiator {
                 factory.parser().parseUncompressedPointRepresentation(
                         entry.getKeyExchange().bytes(), 
                         getCoordinateLength(getGroup()));
-        
-        return new ECPoint(new BigInteger(upr.getX()), new BigInteger(upr.getY()));
+
+        // TODO: is it correct to use BigInteger(int, byte[]) to set the sign +1
+        //       instead of BigInteger(byte[]) ?
+        return new ECPoint(
+                new BigInteger(1, upr.getX()),
+                new BigInteger(1, upr.getY()));
     }
     
     private UncompressedPointRepresentation createUPR(ECPoint point) {
@@ -89,6 +101,53 @@ public class ECDHENegotiator extends AbstractNegotiator {
         return factory.createUncompressedPointRepresentation(
                 toBytes(point.getAffineX(), coordinate_length), 
                 toBytes(point.getAffineY(), coordinate_length));
+    }
+
+    // validates an EC public key defined by section 5.6.2.3.2 of NIST SP 56A
+    // and section 5.2.2 of X9.62:
+    // - https://nvlpubs.nist.gov/nistpubs/SpecialPublications/NIST.SP.800-56Ar2.pdf
+    // - http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.202.2977&rep=rep1&type=pdf
+    private void validate(ECPoint point) throws NegotiatorException {
+        // step #1: verify the public key is not the point at infinity
+        BigInteger x = point.getAffineX();
+        BigInteger y = point.getAffineY();
+        if (x == null || y == null) {
+            output.info("x = %s", x.toString());
+            output.info("y = %s", y.toString());
+            //throw new NegotiatorException("point is at infinity");
+        }
+
+        // step #2: verify x and y are in range [0, p-1]
+        BigInteger p = getP();
+        if (x.signum() == -1 || x.compareTo(p) != -1) {
+            output.achtung("x = %s", x.toString());
+            output.achtung("p = %s", p.toString());
+            throw new NegotiatorException("x is out of range [0, p-1]");
+        }
+        if (y.signum() == -1 || y.compareTo(p) != -1) {
+            output.achtung("y = %s", y.toString());
+            output.achtung("p = %s", p.toString());
+            throw new NegotiatorException("y is out of range [0, p-1]");
+        }
+
+        // step #3: verify that y^2 == x^3 + ax + b (mod p)
+        BigInteger a = secpParameters.ecParameterSpec.getCurve().getA();
+        BigInteger b = secpParameters.ecParameterSpec.getCurve().getA();
+        BigInteger lhs = y.multiply(y).mod(p);
+        BigInteger rhs = x.multiply(x).add(a).multiply(x).add(b).mod(p);
+        if (!lhs.equals(rhs)) {
+            output.achtung("point is not on the curve");
+            throw new NegotiatorException("point is not on the curve");
+        }
+    }
+
+    private BigInteger getP() throws NegotiatorException {
+        ECField field = secpParameters.ecParameterSpec.getCurve().getField();
+        if (field instanceof ECFieldFp) {
+            return ((ECFieldFp) field).getP();
+        }
+
+        throw new NegotiatorException("only curves over prime order fields are supported");
     }
     
     public static ECDHENegotiator create(NamedGroup.Secp group, StructFactory factory) 
@@ -150,5 +209,3 @@ public class ECDHENegotiator extends AbstractNegotiator {
     }
 
 }
-
-
