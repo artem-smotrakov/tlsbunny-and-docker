c1b0b66a46e882e8bfd2aa07aeb403d9501b6766
Artem Smotrakov <artem.smotrakov@gmail.com>
2018-05-10 14:58:35 +0200

    Implemented WeakECDHE for h2o

--- src/main/java/com/gypsyengineer/tlsbunny/tls13/test/h2o/client/WeakECDHE.java
+++ src/main/java/com/gypsyengineer/tlsbunny/tls13/test/h2o/client/WeakECDHE.java
@@ -1,25 +1,125 @@
 package com.gypsyengineer.tlsbunny.tls13.test.h2o.client;
 
-import com.gypsyengineer.tlsbunny.utils.ECException;
-import com.gypsyengineer.tlsbunny.utils.ECUtils;
-import com.gypsyengineer.tlsbunny.utils.MathException;
+import com.gypsyengineer.tlsbunny.tls13.connection.AlertCheck;
+import com.gypsyengineer.tlsbunny.tls13.connection.Engine;
+import com.gypsyengineer.tlsbunny.tls13.connection.action.composite.IncomingChangeCipherSpec;
+import com.gypsyengineer.tlsbunny.tls13.connection.action.simple.*;
+import com.gypsyengineer.tlsbunny.tls13.handshake.Context;
+import com.gypsyengineer.tlsbunny.tls13.handshake.Negotiator;
+import com.gypsyengineer.tlsbunny.tls13.handshake.WeakECDHENegotiator;
+import com.gypsyengineer.tlsbunny.tls13.struct.StructFactory;
+import com.gypsyengineer.tlsbunny.tls13.test.CommonConfig;
+import com.gypsyengineer.tlsbunny.utils.Output;
 
-import java.math.BigInteger;
-import java.security.GeneralSecurityException;
-import java.security.InvalidAlgorithmParameterException;
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
-import java.security.interfaces.ECPublicKey;
-import java.security.spec.ECParameterSpec;
-import java.security.spec.ECPoint;
-import java.security.spec.ECPublicKeySpec;
-import java.security.spec.EllipticCurve;
-
-import static com.gypsyengineer.tlsbunny.utils.ECUtils.checkPointOnCurve;
-import static com.gypsyengineer.tlsbunny.utils.MathUtils.THREE;
-import static com.gypsyengineer.tlsbunny.utils.MathUtils.modSqrt;
+import static com.gypsyengineer.tlsbunny.tls13.struct.ContentType.handshake;
+import static com.gypsyengineer.tlsbunny.tls13.struct.HandshakeType.*;
+import static com.gypsyengineer.tlsbunny.tls13.struct.HandshakeType.certificate_verify;
+import static com.gypsyengineer.tlsbunny.tls13.struct.HandshakeType.finished;
+import static com.gypsyengineer.tlsbunny.tls13.struct.NamedGroup.secp256r1;
+import static com.gypsyengineer.tlsbunny.tls13.struct.ProtocolVersion.TLSv12;
+import static com.gypsyengineer.tlsbunny.tls13.struct.ProtocolVersion.TLSv13_draft_26;
+import static com.gypsyengineer.tlsbunny.tls13.struct.SignatureScheme.ecdsa_secp256r1_sha256;
 
 public class WeakECDHE {
 
+    public static void main(String[] args) throws Exception {
+        CommonConfig config = CommonConfig.load();
 
+        StructFactory factory = StructFactory.getDefault();
+        Negotiator negotiator = WeakECDHENegotiator.create(secp256r1, factory);
+
+        try (Output output = new Output()) {
+            Engine.init()
+                    .target(config.host())
+                    .target(config.port())
+
+                    .set(output)
+                    .set(factory)
+                    .set(negotiator)
+
+                    // send ClientHello
+                    .run(new GeneratingClientHello()
+                            .supportedVersion(TLSv13_draft_26)
+                            .group(secp256r1)
+                            .signatureScheme(ecdsa_secp256r1_sha256)
+                            .keyShareEntry(context -> context.negotiator.createKeyShareEntry()))
+                    .run(new WrappingIntoHandshake()
+                            .type(client_hello)
+                            .updateContext(Context.Element.first_client_hello))
+                    .run(new WrappingIntoTLSPlaintexts()
+                            .type(handshake)
+                            .version(TLSv12))
+                    .send(new OutgoingData())
+
+                    // receive ServerHello, EncryptedExtensions, Certificate,
+                    // CertificateVerify and Finished messages
+                    .require(new IncomingData())
+
+                    // process ServerHello
+                    .run(new ProcessingTLSPlaintext()
+                            .expect(handshake))
+                    .run(new ProcessingHandshake()
+                            .expect(server_hello)
+                            .updateContext(Context.Element.server_hello))
+                    .run(new ProcessingServerHello())
+                    .run(new NegotiatingDHSecret())
+                    .run(new ComputingKeysAfterServerHello())
+
+                    .allow(new IncomingChangeCipherSpec())
+
+                    // process EncryptedExtensions
+                    .run(new ProcessingHandshakeTLSCiphertext()
+                            .expect(handshake))
+                    .run(new ProcessingHandshake()
+                            .expect(encrypted_extensions)
+                            .updateContext(Context.Element.encrypted_extensions))
+                    .run(new ProcessingEncryptedExtensions())
+
+                    // process Certificate
+                    .run(new ProcessingHandshakeTLSCiphertext()
+                            .expect(handshake))
+                    .run(new ProcessingHandshake()
+                            .expect(certificate)
+                            .updateContext(Context.Element.server_certificate))
+                    .run(new ProcessingCertificate())
+
+                    // process CertificateVerify
+                    .run(new ProcessingHandshakeTLSCiphertext()
+                            .expect(handshake))
+                    .run(new ProcessingHandshake()
+                            .expect(certificate_verify)
+                            .updateContext(Context.Element.server_certificate_verify))
+                    .run(new ProcessingCertificateVerify())
+
+                    // process Finished
+                    .run(new ProcessingHandshakeTLSCiphertext())
+                    .run(new ProcessingHandshake()
+                            .expect(finished)
+                            .updateContext(Context.Element.server_finished))
+                    .run(new ProcessingFinished())
+                    .run(new ComputingKeysAfterServerFinished())
+
+                    // send Finished
+                    .run(new GeneratingFinished())
+                    .run(new ComputingKeysAfterClientFinished())
+                    .run(new WrappingIntoHandshake()
+                            .type(finished)
+                            .updateContext(Context.Element.client_finished))
+                    .run(new WrappingHandshakeDataIntoTLSCiphertext())
+                    .send(new OutgoingData())
+
+                    // send application data
+                    .run(new PreparingHttpGetRequest())
+                    .run(new WrappingApplicationDataIntoTLSCiphertext())
+                    .send(new OutgoingData())
+
+                    // receive application data
+                    .require(new IncomingData())
+                    .run(new ProcessingApplicationDataTLSCiphertext())
+                    .run(new PrintingData())
+
+                    .connect()
+                    .run(new AlertCheck());
+        }
+    }
 }
