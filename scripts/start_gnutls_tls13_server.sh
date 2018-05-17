bdec699d954564401391066b12690c5803806d29
Artem Smotrakov <artem.smotrakov@gmail.com>
2018-05-17 23:54:21 +0200

    Disable client auth for GnuTLS server

--- scripts/start_gnutls_tls13_server.sh
+++ scripts/start_gnutls_tls13_server.sh
@@ -4,4 +4,5 @@ gnutls-serv --port=50101 --debug=9999 --http \
     --x509cafile certs/root_cert.pem \
     --x509keyfile certs/server_key.pem \
     --x509certfile certs/server_cert.pem \
+    --disable-client-cert \
     ${OPTIONS}
