ef549e3dcdf5271fb30dc5850b8886fe2bb0380c
Artem Smotrakov <artem.smotrakov@gmail.com>
2018-05-21 22:56:00 +0200

    Better running a WolfSSL server with Docker

--- scripts/start_wolfssl_tls13_server.sh
+++ scripts/start_wolfssl_tls13_server.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
-./examples/server/server \
-    -p 40101 \
-    -v 4 \
-    -d -i -g -b -x \
-    -c certs/server-cert.pem \
-    -k certs/server-key.pem
+# looks like wolfssl server can exit if a wrong message was received
+# so run it in a loop
+while true;
+do
+    ./examples/server/server \
+        -p 40101 \
+        -v 4 \
+        -d -i -g -b -x \
+        -c certs/server-cert.pem \
+        -k certs/server-key.pem > log 2>&1
+
+    # print output
+    cat log
+
+    # exit if ASan found something
+    if grep AddressSanitizer log > /dev/null 2>&1; then
+        exit 1
+    fi
+done
