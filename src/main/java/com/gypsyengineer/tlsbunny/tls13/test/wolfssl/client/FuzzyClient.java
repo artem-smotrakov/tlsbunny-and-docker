707dd570a6c9c3b3b33d2538350a87b4691ec6bb
Artem Smotrakov <artem.smotrakov@gmail.com>
2018-05-21 22:26:25 +0200

    Added a client and fuzzers for WolfSSL

---
+++ src/main/java/com/gypsyengineer/tlsbunny/tls13/test/wolfssl/client/FuzzyClient.java
@@ -0,0 +1,18 @@
+package com.gypsyengineer.tlsbunny.tls13.test.wolfssl.client;
+
+import com.gypsyengineer.tlsbunny.tls13.connection.NoAlertAnalyzer;
+import com.gypsyengineer.tlsbunny.tls13.test.common.client.MultipleThreads;
+
+public class FuzzyClient {
+
+    public static void main(String[] args) throws InterruptedException {
+        new MultipleThreads()
+                .add(TLSPlaintextFuzzer.factory, TLSPlaintextFuzzer.tls_plaintext_configs)
+                .add(CCSFuzzer.factory, CCSFuzzer.ccs_configs)
+                .add(HandshakeFuzzer.factory, HandshakeFuzzer.handshake_configs)
+                .add(ClientHelloFuzzer.factory, ClientHelloFuzzer.client_hello_configs)
+                .add(FinishedFuzzer.factory, FinishedFuzzer.finished_configs)
+                .set(new NoAlertAnalyzer())
+                .submit();
+    }
+}
