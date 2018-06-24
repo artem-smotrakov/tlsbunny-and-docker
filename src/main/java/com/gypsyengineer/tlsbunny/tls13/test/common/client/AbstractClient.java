8647fffd80a8f95e518cda0fb3c263aa8808c816
Artem Smotrakov <artem.smotrakov@gmail.com>
2018-06-24 13:07:23 +0200

    Make AbstractClient AutoClosable

--- src/main/java/com/gypsyengineer/tlsbunny/tls13/test/common/client/AbstractClient.java
+++ src/main/java/com/gypsyengineer/tlsbunny/tls13/test/common/client/AbstractClient.java
@@ -2,13 +2,18 @@ package com.gypsyengineer.tlsbunny.tls13.test.common.client;
 
 import com.gypsyengineer.tlsbunny.tls13.struct.StructFactory;
 import com.gypsyengineer.tlsbunny.tls13.test.Config;
+import com.gypsyengineer.tlsbunny.tls13.test.SystemPropertiesConfig;
 import com.gypsyengineer.tlsbunny.utils.Output;
 
-public abstract class AbstractClient implements Client {
+public abstract class AbstractClient implements Client, AutoCloseable {
 
-    protected Config config;
-    protected StructFactory factory;
-    protected Output output;
+    protected Config config = SystemPropertiesConfig.load();
+    protected StructFactory factory = StructFactory.getDefault();
+    protected Output output = new Output();
+
+    public Config config() {
+        return config;
+    }
 
     @Override
     public Client set(Config config) {
@@ -26,5 +31,12 @@ public abstract class AbstractClient implements Client {
     public Client set(Output output) {
         this.output = output;
         return this;
+    }
+
+    @Override
+    public void close() {
+        if (output != null) {
+            output.flush();
+        }
     }
 }
