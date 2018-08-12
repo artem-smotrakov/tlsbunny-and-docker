d9d02a164a958509a9ccfa022543c59be113664a
Artem Smotrakov <artem.smotrakov@gmail.com>
2018-08-12 19:37:24 +0200

    Removed unused field in AnotherHttpsClient

--- src/main/java/com/gypsyengineer/tlsbunny/tls13/test/openssl/client/AnotherHttpsClient.java
+++ src/main/java/com/gypsyengineer/tlsbunny/tls13/test/openssl/client/AnotherHttpsClient.java
@@ -2,17 +2,12 @@ package com.gypsyengineer.tlsbunny.tls13.test.openssl.client;
 
 import com.gypsyengineer.tlsbunny.tls13.connection.*;
 import com.gypsyengineer.tlsbunny.tls13.connection.action.composite.*;
-import com.gypsyengineer.tlsbunny.tls13.struct.ProtocolVersion;
 import com.gypsyengineer.tlsbunny.tls13.struct.StructFactory;
 import com.gypsyengineer.tlsbunny.tls13.test.SystemPropertiesConfig;
 import com.gypsyengineer.tlsbunny.tls13.test.common.client.AbstractClient;
 import com.gypsyengineer.tlsbunny.utils.Output;
 
-import static com.gypsyengineer.tlsbunny.tls13.struct.ProtocolVersion.TLSv13_draft_26;
-
 public class AnotherHttpsClient extends AbstractClient {
-
-    private ProtocolVersion protocolVersion = TLSv13_draft_26;
 
     public static void main(String[] args) throws Exception {
         try (Output output = new Output()) {
@@ -23,11 +18,6 @@ public class AnotherHttpsClient extends AbstractClient {
                     .connect()
                     .run(new NoAlertCheck());
         }
-    }
-
-    public AnotherHttpsClient version(ProtocolVersion protocolVersion) {
-        this.protocolVersion = protocolVersion;
-        return this;
     }
 
     @Override
