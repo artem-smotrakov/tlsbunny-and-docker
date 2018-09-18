9aa47662a4254145ab9ca6121db5fc5b287b7d51
Artem Smotrakov <artem.smotrakov@gmail.com>
2018-09-18 23:44:25 +0200

    First draft of OpensslTestSuite

---
+++ src/impl/test/com/gypsyengineer/tlsbunny/impl/test/tls13/openssl/OpensslTestSuite.java
@@ -0,0 +1,33 @@
+package com.gypsyengineer.tlsbunny.impl.test.tls13.openssl;
+
+import com.gypsyengineer.tlsbunny.impl.test.tls13.TestForServer;
+import com.gypsyengineer.tlsbunny.impl.test.tls13.Utils;
+import com.gypsyengineer.tlsbunny.tls13.client.HttpsClient;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class OpensslTestSuite {
+
+    private static OpensslServer server;
+
+    @BeforeClass
+    public static void setup() throws Exception {
+        server = new OpensslServer();
+        server.start();
+        Utils.waitServerStart(server);
+    }
+
+    @Test
+    public void httpClient() throws Exception {
+        new TestForServer()
+                .set(new HttpsClient())
+                .set(server).run();
+    }
+
+    @AfterClass
+    public static void shutdown() throws Exception {
+        server.stop();
+        Utils.waitServerStop(server);
+    }
+}
