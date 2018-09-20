ba6cf63da0de771ac12b6ebbec4600042fbf1736
Artem Smotrakov <artem.smotrakov@gmail.com>
2018-09-20 22:34:01 +0200

    Make CCSAfterHandshake great again

--- src/main/core/com/gypsyengineer/tlsbunny/tls13/client/CCSAfterHandshake.java
+++ src/main/core/com/gypsyengineer/tlsbunny/tls13/client/CCSAfterHandshake.java
@@ -1,30 +1,93 @@
 package com.gypsyengineer.tlsbunny.tls13.client;
 
 import com.gypsyengineer.tlsbunny.tls13.connection.*;
+import com.gypsyengineer.tlsbunny.tls13.connection.action.Side;
 import com.gypsyengineer.tlsbunny.tls13.connection.action.composite.*;
+import com.gypsyengineer.tlsbunny.tls13.connection.action.simple.*;
+import com.gypsyengineer.tlsbunny.tls13.handshake.Context;
+import com.gypsyengineer.tlsbunny.tls13.handshake.NegotiatorException;
+import com.gypsyengineer.tlsbunny.tls13.struct.StructFactory;
+import com.gypsyengineer.tlsbunny.utils.Output;
 import com.gypsyengineer.tlsbunny.utils.SystemPropertiesConfig;
 
-public class CCSAfterHandshake {
+import java.security.NoSuchAlgorithmException;
+import java.util.List;
+
+import static com.gypsyengineer.tlsbunny.tls13.struct.ContentType.handshake;
+import static com.gypsyengineer.tlsbunny.tls13.struct.HandshakeType.client_hello;
+import static com.gypsyengineer.tlsbunny.tls13.struct.HandshakeType.finished;
+import static com.gypsyengineer.tlsbunny.tls13.struct.NamedGroup.secp256r1;
+import static com.gypsyengineer.tlsbunny.tls13.struct.ProtocolVersion.TLSv12;
+import static com.gypsyengineer.tlsbunny.tls13.struct.ProtocolVersion.TLSv13;
+import static com.gypsyengineer.tlsbunny.tls13.struct.SignatureScheme.ecdsa_secp256r1_sha256;
+
+public class CCSAfterHandshake extends AbstractClient {
 
     public static void main(String[] args) throws Exception {
-        SystemPropertiesConfig config = SystemPropertiesConfig.load();
-
-        Engine.init()
-                .target(config.host())
-                .target(config.port())
-                .send(new OutgoingClientHello())
-                .send(new OutgoingChangeCipherSpec())
-                .receive(new IncomingServerHello())
-                .receive(new IncomingChangeCipherSpec())
-                .receive(new IncomingEncryptedExtensions())
-                .receive(new IncomingCertificate())
-                .receive(new IncomingCertificateVerify())
-                .receive(new IncomingFinished())
-                .send(new OutgoingFinished())
-                .send(new OutgoingChangeCipherSpec())
-                .receive(new IncomingAlert())
-                .connect()
-                .run(new AlertCheck());
+        try (Output output = new Output()) {
+            new HttpsClient()
+                    .set(SystemPropertiesConfig.load())
+                    .set(StructFactory.getDefault())
+                    .set(output)
+                    .connect();
+        }
     }
 
+    @Override
+    protected Engine createEngine()
+            throws NegotiatorException, NoSuchAlgorithmException {
+
+        return Engine.init()
+                .target(config.host())
+                .target(config.port())
+                .set(factory)
+                .set(output)
+
+                // send ClientHello
+                .run(new GeneratingClientHello()
+                        .supportedVersions(TLSv13)
+                        .groups(secp256r1)
+                        .signatureSchemes(ecdsa_secp256r1_sha256)
+                        .keyShareEntries(context -> context.negotiator.createKeyShareEntry()))
+                .run(new WrappingIntoHandshake()
+                        .type(client_hello)
+                        .updateContext(Context.Element.first_client_hello))
+                .run(new WrappingIntoTLSPlaintexts()
+                        .type(handshake)
+                        .version(TLSv12))
+                .send(new OutgoingData())
+
+                // send an expected CCS
+                .send(new OutgoingChangeCipherSpec())
+
+                // receive a ServerHello, EncryptedExtensions, Certificate,
+                // CertificateVerify and Finished messages
+                .loop(context -> !context.hasServerFinished() && !context.hasAlert())
+                    .receive(() -> new IncomingMessages(Side.client))
+
+                // send Finished
+                .run(new GeneratingFinished())
+                .run(new WrappingIntoHandshake()
+                        .type(finished)
+                        .updateContext(Context.Element.client_finished))
+                .run(new WrappingHandshakeDataIntoTLSCiphertext())
+                .send(new OutgoingData())
+
+                // send an unexpected CCS
+                .send(new OutgoingChangeCipherSpec())
+
+                // send application data
+                .run(new PreparingHttpGetRequest())
+                .run(new WrappingApplicationDataIntoTLSCiphertext())
+                .send(new OutgoingData())
+
+                // receive session tickets and application data
+                .loop(context -> !context.receivedApplicationData() && !context.hasAlert())
+                    .receive(() -> new IncomingMessages(Side.client));
+    }
+
+    @Override
+    protected List<Check> createChecks() {
+        return List.of(new AlertCheck());
+    }
 }
