54aed1328ce7d3e92088db212c07070e32cda071
Artem Smotrakov <artem.smotrakov@gmail.com>
2018-10-06 00:17:45 +0200

    Added Struct.cast() method

--- src/main/core/com/gypsyengineer/tlsbunny/tls/Struct.java
+++ src/main/core/com/gypsyengineer/tlsbunny/tls/Struct.java
@@ -2,6 +2,8 @@ package com.gypsyengineer.tlsbunny.tls;
 
 import java.io.IOException;
 
+import static com.gypsyengineer.tlsbunny.utils.WhatTheHell.whatTheHell;
+
 /**
  * TLS structure.
  */
@@ -48,6 +50,14 @@ public interface Struct {
      */
     default void element(int index, Struct element) {
         throw new UnsupportedOperationException("no setting elements for you!");
+    }
+
+    static <T> T cast(Struct object, Class<T> clazz) {
+        if (!clazz.isAssignableFrom(object.getClass())) {
+            throw whatTheHell("expected %s but received %s",
+                    clazz.getSimpleName(), object.getClass().getSimpleName());
+        }
+        return (T) object;
     }
 
 }
