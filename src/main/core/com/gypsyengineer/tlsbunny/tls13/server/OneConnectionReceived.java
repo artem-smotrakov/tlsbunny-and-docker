1be8b295971f77a3195d8966fa3d7d370cf12f2a
Artem Smotrakov <artem.smotrakov@gmail.com>
2018-11-04 14:55:05 +0100

    Added a factory method for OneConnectionReceived

--- src/main/core/com/gypsyengineer/tlsbunny/tls13/server/OneConnectionReceived.java
+++ src/main/core/com/gypsyengineer/tlsbunny/tls13/server/OneConnectionReceived.java
@@ -2,6 +2,10 @@ package com.gypsyengineer.tlsbunny.tls13.server;
 
 public class OneConnectionReceived extends NConnectionsReceived {
 
+    public static OneConnectionReceived oneConnectionReceived() {
+        return new OneConnectionReceived();
+    }
+
     public OneConnectionReceived() {
         super(1);
     }
