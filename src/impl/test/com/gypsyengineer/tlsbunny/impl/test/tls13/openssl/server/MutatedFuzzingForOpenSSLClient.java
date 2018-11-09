d3ed812a94b890b8a7c3971deec2b8aaecd509f8
Artem Smotrakov <artem.smotrakov@gmail.com>
2018-11-10 00:40:35 +0100

    Added first draft of MutatedFuzzingForOpenSSLClient

---
+++ src/impl/test/com/gypsyengineer/tlsbunny/impl/test/tls13/openssl/server/MutatedFuzzingForOpenSSLClient.java
@@ -0,0 +1,27 @@
+package com.gypsyengineer.tlsbunny.impl.test.tls13.openssl.server;
+
+import com.gypsyengineer.tlsbunny.impl.test.tls13.ImplTest;
+import com.gypsyengineer.tlsbunny.tls13.client.fuzzer.MultiThreadedClient;
+import com.gypsyengineer.tlsbunny.tls13.server.fuzzer.MutatedServer;
+import com.gypsyengineer.tlsbunny.utils.Config;
+import com.gypsyengineer.tlsbunny.utils.SystemPropertiesConfig;
+import org.junit.Test;
+
+import static com.gypsyengineer.tlsbunny.tls13.server.HttpsServer.httpsServer;
+import static com.gypsyengineer.tlsbunny.tls13.server.OneConnectionReceived.oneConnectionReceived;
+import static com.gypsyengineer.tlsbunny.tls13.struct.NamedGroup.secp256r1;
+
+public class MutatedFuzzingForOpenSSLClient {
+
+    private static Config serverConfig = SystemPropertiesConfig.load();
+
+    @Test
+    // TODO check for ASan messages
+    public void ccs() throws Exception {
+        new ImplTest()
+                .set(new OpensslClient())
+                .set(new MutatedServer(httpsServer().set(secp256r1))
+                        .stopWhen(oneConnectionReceived()))
+                .run();
+    }
+}
