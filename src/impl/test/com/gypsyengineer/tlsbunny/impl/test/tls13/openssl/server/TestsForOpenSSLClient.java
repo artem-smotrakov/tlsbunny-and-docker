f594207f14bc7626423000aea0cdb27531fc204c
Artem Smotrakov <artem.smotrakov@gmail.com>
2018-11-10 00:18:06 +0100

    Use HttpsServer in TestsForOpenSSLClient

--- src/impl/test/com/gypsyengineer/tlsbunny/impl/test/tls13/openssl/server/TestsForOpenSSLClient.java
+++ src/impl/test/com/gypsyengineer/tlsbunny/impl/test/tls13/openssl/server/TestsForOpenSSLClient.java
@@ -1,30 +1,13 @@
 package com.gypsyengineer.tlsbunny.impl.test.tls13.openssl.server;
 
 import com.gypsyengineer.tlsbunny.impl.test.tls13.ImplTest;
-import com.gypsyengineer.tlsbunny.tls13.connection.BaseEngineFactory;
-import com.gypsyengineer.tlsbunny.tls13.connection.Engine;
-import com.gypsyengineer.tlsbunny.tls13.connection.action.Side;
-import com.gypsyengineer.tlsbunny.tls13.connection.action.composite.OutgoingChangeCipherSpec;
-import com.gypsyengineer.tlsbunny.tls13.connection.action.simple.*;
-import com.gypsyengineer.tlsbunny.tls13.handshake.Context;
-import com.gypsyengineer.tlsbunny.tls13.handshake.Negotiator;
-import com.gypsyengineer.tlsbunny.tls13.server.SingleThreadServer;
-import com.gypsyengineer.tlsbunny.tls13.struct.StructFactory;
 import com.gypsyengineer.tlsbunny.utils.Config;
 import com.gypsyengineer.tlsbunny.utils.SystemPropertiesConfig;
 import org.junit.Test;
 
+import static com.gypsyengineer.tlsbunny.tls13.server.HttpsServer.httpsServer;
 import static com.gypsyengineer.tlsbunny.tls13.server.OneConnectionReceived.oneConnectionReceived;
-import static com.gypsyengineer.tlsbunny.tls13.struct.ContentType.application_data;
-import static com.gypsyengineer.tlsbunny.tls13.struct.ContentType.change_cipher_spec;
-import static com.gypsyengineer.tlsbunny.tls13.struct.ContentType.handshake;
-import static com.gypsyengineer.tlsbunny.tls13.struct.HandshakeType.*;
-import static com.gypsyengineer.tlsbunny.tls13.struct.HandshakeType.certificate_verify;
-import static com.gypsyengineer.tlsbunny.tls13.struct.HandshakeType.finished;
 import static com.gypsyengineer.tlsbunny.tls13.struct.NamedGroup.secp256r1;
-import static com.gypsyengineer.tlsbunny.tls13.struct.ProtocolVersion.TLSv12;
-import static com.gypsyengineer.tlsbunny.tls13.struct.ProtocolVersion.TLSv13;
-import static com.gypsyengineer.tlsbunny.tls13.struct.SignatureScheme.ecdsa_secp256r1_sha256;
 
 /**
  * Tests for OpenSSL s_client.
@@ -37,133 +20,10 @@ public class TestsForOpenSSLClient {
 
         new ImplTest()
                 .set(new OpensslClient())
-                .set(new SingleThreadServer()
+                .set(httpsServer()
                         .set(serverConfig)
-                        .set(new EngineFactoryImpl()
-                                .set(serverConfig)
-                                .set(Negotiator.create(
-                                        secp256r1,
-                                        StructFactory.getDefault())))
+                        .set(secp256r1)
                         .stopWhen(oneConnectionReceived()))
                 .run();
-    }
-
-    private static class EngineFactoryImpl extends BaseEngineFactory {
-
-        private Negotiator negotiator;
-        private Config config;
-
-        public EngineFactoryImpl set(Config config) {
-            this.config = config;
-            return this;
-        }
-
-        public EngineFactoryImpl set(Negotiator negotiator) {
-            this.negotiator = negotiator;
-            return this;
-        }
-
-        @Override
-        protected Engine createImpl() throws Exception {
-            return Engine.init()
-                    .set(structFactory)
-                    .set(output)
-                    .set(negotiator)
-                    .set(negotiator.group())
-
-                    .receive(new IncomingData())
-
-                    // process ClientHello
-                    .run(new ProcessingTLSPlaintext()
-                            .expect(handshake))
-                    .run(new ProcessingHandshake()
-                            .expect(client_hello)
-                            .updateContext(Context.Element.first_client_hello))
-                    .run(new ProcessingClientHello())
-
-                    // send ServerHello
-                    .run(new GeneratingServerHello()
-                            .supportedVersion(TLSv13)
-                            .group(secp256r1)
-                            .signatureScheme(ecdsa_secp256r1_sha256)
-                            .keyShareEntry(context -> context.negotiator.createKeyShareEntry()))
-                    .run(new WrappingIntoHandshake()
-                            .type(server_hello)
-                            .updateContext(Context.Element.server_hello))
-                    .run(new WrappingIntoTLSPlaintexts()
-                            .type(handshake)
-                            .version(TLSv12))
-                    .store()
-
-                    .run(new OutgoingChangeCipherSpec())
-                    .store()
-
-                    .run(new NegotiatingServerDHSecret())
-
-                    .run(new ComputingHandshakeTrafficKeys()
-                            .server())
-
-                    // send EncryptedExtensions
-                    .run(new GeneratingEncryptedExtensions())
-                    .run(new WrappingIntoHandshake()
-                            .type(encrypted_extensions)
-                            .updateContext(Context.Element.encrypted_extensions))
-                    .run(new WrappingHandshakeDataIntoTLSCiphertext())
-                    .store()
-
-                    // send Certificate
-                    .run(new GeneratingCertificate()
-                            .certificate(config.serverCertificate()))
-                    .run(new WrappingIntoHandshake()
-                            .type(certificate)
-                            .updateContext(Context.Element.server_certificate))
-                    .run(new WrappingHandshakeDataIntoTLSCiphertext())
-                    .store()
-
-                    // send CertificateVerify
-                    .run(new GeneratingCertificateVerify()
-                            .server()
-                            .key(config.serverKey()))
-                    .run(new WrappingIntoHandshake()
-                            .type(certificate_verify)
-                            .updateContext(Context.Element.server_certificate_verify))
-                    .run(new WrappingHandshakeDataIntoTLSCiphertext())
-                    .store()
-
-                    .run(new GeneratingFinished(Side.server))
-                    .run(new WrappingIntoHandshake()
-                            .type(finished)
-                            .updateContext(Context.Element.server_finished))
-                    .run(new WrappingHandshakeDataIntoTLSCiphertext())
-                    .store()
-
-                    .restore()
-                    .send(new OutgoingData())
-
-                    .receive(new IncomingData())
-
-                    .run(new ProcessingTLSPlaintext().expect(change_cipher_spec))
-                    .run(new ProcessingChangeCipherSpec())
-
-                    .run(new ProcessingHandshakeTLSCiphertext()
-                            .expect(handshake))
-                    .run(new ProcessingHandshake()
-                            .expect(finished))
-                    .run(new ProcessingFinished(Side.server))
-
-                    .run(new ComputingApplicationTrafficKeys()
-                            .server())
-
-                    // receive application data
-                    .receive(new IncomingData())
-                    .run(new ProcessingApplicationDataTLSCiphertext()
-                            .expect(application_data))
-                    .run(new PrintingData())
-
-                    // send application data
-                    .run(new PreparingHttpResponse())
-                    .run(new WrappingApplicationDataIntoTLSCiphertext())
-                    .send(new OutgoingData());
-        }
     }
 }
