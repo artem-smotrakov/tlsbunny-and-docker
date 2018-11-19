a7da5b187773e03d3a3f0bb8affdefd3bec35672
Artem Smotrakov <artem.smotrakov@gmail.com>
2018-11-19 22:50:06 +0100

    Better docker image for wolfssl

--- src/vendor/wolfssl/start.sh
+++ src/vendor/wolfssl/start.sh
@@ -2,20 +2,15 @@
 
 # looks like wolfssl server can exit if a wrong message was received
 # so run it in a loop
-while true;
+
+touch stop.file
+echo "note: remove $(pwd)/stop.file to stop the server loop"
+while [ -f stop.file ];
 do
     ./examples/server/server \
         -p 40101 \
         -v 4 \
         -d -i -g -b -x \
         -c certs/server-cert.pem \
-        -k certs/server-key.pem > log 2>&1
-
-    # print output
-    cat log
-
-    # exit if ASan found something
-    if grep AddressSanitizer log > /dev/null 2>&1; then
-        exit 1
-    fi
+        -k certs/server-key.pem 2>&1
 done
