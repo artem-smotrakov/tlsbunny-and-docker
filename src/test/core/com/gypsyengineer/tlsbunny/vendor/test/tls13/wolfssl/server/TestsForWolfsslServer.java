ff9ab337163dc9d7159055ae767652615958e0bc
Artem Smotrakov <artem.smotrakov@gmail.com>
2018-11-20 08:53:20 +0100

    Added TestsForWolfsslServer

---
+++ src/test/core/com/gypsyengineer/tlsbunny/vendor/test/tls13/wolfssl/server/TestsForWolfsslServer.java
@@ -0,0 +1,18 @@
+package com.gypsyengineer.tlsbunny.vendor.test.tls13.wolfssl.server;
+
+import com.gypsyengineer.tlsbunny.vendor.test.tls13.TestsForServer;
+import com.gypsyengineer.tlsbunny.vendor.test.tls13.Utils;
+import org.junit.BeforeClass;
+
+import static com.gypsyengineer.tlsbunny.vendor.test.tls13.wolfssl.WolfsslServer.wolfsslServer;
+
+public class TestsForWolfsslServer extends TestsForServer {
+
+    @BeforeClass
+    public static void setUp() throws Exception {
+        server = wolfsslServer();
+        server.start();
+        Utils.waitStart(server);
+    }
+
+}
