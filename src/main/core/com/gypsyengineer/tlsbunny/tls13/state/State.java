bacbaba5113a006fa237e59e4adec21e9572d937
Artem Smotrakov <artem.smotrakov@gmail.com>
2018-11-25 21:37:03 +0100

    Added equals() and hashCode() for State

--- src/main/core/com/gypsyengineer/tlsbunny/tls13/state/State.java
+++ src/main/core/com/gypsyengineer/tlsbunny/tls13/state/State.java
@@ -52,4 +52,22 @@ public class State {
 
         return array;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        State state = (State) o;
+        return Objects.equals(facts, state.facts);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(facts);
+    }
 }
