8709b94fbbc4f84ed5fa098cf79b1fb04388e5d2
Artem Smotrakov <artem.smotrakov@gmail.com>
2019-01-27 21:31:16 +0100

    Added FileOutput

---
+++ src/main/core/com/gypsyengineer/tlsbunny/utils/FileOutput.java
@@ -0,0 +1,39 @@
+package com.gypsyengineer.tlsbunny.utils;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.List;
+
+import static com.gypsyengineer.tlsbunny.utils.WhatTheHell.whatTheHell;
+
+public class FileOutput extends AbstractOutput {
+
+    private final Writer writer;
+    private int index = 0;
+
+    public FileOutput(Output output, String filename) {
+        super(output);
+
+        try {
+            writer = new BufferedWriter(new FileWriter(filename));
+        } catch (IOException e) {
+            throw whatTheHell("could not create a file", e);
+        }
+    }
+
+    @Override
+    public void flush() {
+        output.flush();
+
+        try {
+            List<String> lines = output.lines();
+            for (;index < lines.size(); index++) {
+                writer.write(lines.get(index));
+            }
+        } catch (IOException e) {
+            throw whatTheHell("could not write to a file", e);
+        }
+    }
+}
