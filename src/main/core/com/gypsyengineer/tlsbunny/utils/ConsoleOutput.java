2e3bdaf070808cb10feb620fc1ada664f39047e1
Artem Smotrakov <artem.smotrakov@gmail.com>
2019-01-27 09:44:27 +0100

    Restore onlyAchtung option

--- src/main/core/com/gypsyengineer/tlsbunny/utils/ConsoleOutput.java
+++ src/main/core/com/gypsyengineer/tlsbunny/utils/ConsoleOutput.java
@@ -11,6 +11,9 @@ public class ConsoleOutput extends AbstractOutput {
     private static final boolean enableHighlighting = Boolean.valueOf(
             System.getProperty("tlsbunny.output.enable.highlighting", "true"));
 
+    private static boolean onlyAchtung = Boolean.valueOf(
+            System.getProperty("tlsbunny.output.only.achtung", "false"));
+
     private int index = 0;
 
     public ConsoleOutput(Output output) {
@@ -23,16 +26,18 @@ public class ConsoleOutput extends AbstractOutput {
             output.flush();
 
             List<String> lines = output.lines();
-            while (index < lines.size()) {
+            for (;index < lines.size(); index++) {
                 String string = lines.get(index);
+
+                if (onlyAchtung && !string.contains("achtung")) {
+                    continue;
+                }
 
                 if (enableHighlighting && string.contains("achtung")) {
                     string = String.format("%s%s%s", ansi_red, string, ansi_reset);
                 }
 
                 System.out.print(string);
-
-                index++;
             }
         }
     }
