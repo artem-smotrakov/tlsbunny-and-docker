b7bb5dc3739aa3d4073e2ef134fe1d1148bd1b91
Artem Smotrakov <artem.smotrakov@gmail.com>
2019-02-23 12:40:51 +0100

    Make KeyShareImpl composite

--- src/main/core/com/gypsyengineer/tlsbunny/tls13/struct/impl/KeyShareImpl.java
+++ src/main/core/com/gypsyengineer/tlsbunny/tls13/struct/impl/KeyShareImpl.java
@@ -1,5 +1,6 @@
 package com.gypsyengineer.tlsbunny.tls13.struct.impl;
 
+import com.gypsyengineer.tlsbunny.tls.Struct;
 import com.gypsyengineer.tlsbunny.tls.Vector;
 import com.gypsyengineer.tlsbunny.tls13.struct.KeyShare;
 import com.gypsyengineer.tlsbunny.tls13.struct.KeyShareEntry;
@@ -7,11 +8,14 @@ import com.gypsyengineer.tlsbunny.tls13.struct.NamedGroup;
 import java.io.IOException;
 import java.util.Objects;
 
+import static com.gypsyengineer.tlsbunny.utils.Utils.cast;
+import static com.gypsyengineer.tlsbunny.utils.WhatTheHell.whatTheHell;
+
 public abstract class KeyShareImpl implements KeyShare {
     
     public static class ClientHelloImpl extends KeyShareImpl implements ClientHello {
 
-        private final Vector<KeyShareEntry> client_shares;
+        private Vector<KeyShareEntry> client_shares;
 
         ClientHelloImpl() {
             this(Vector.wrap(length_bytes));
@@ -57,11 +61,45 @@ public abstract class KeyShareImpl implements KeyShare {
         public int hashCode() {
             return Objects.hash(client_shares);
         }
+
+        @Override
+        public boolean composite() {
+            return true;
+        }
+
+        @Override
+        public int total() {
+            return 1;
+        }
+
+        @Override
+        public Struct element(int index) {
+            switch (index) {
+                case 0:
+                    return client_shares;
+                default:
+                    throw whatTheHell("incorrect index %d!", index);
+            }
+        }
+
+        @Override
+        public void element(int index, Struct element) {
+            if (element == null) {
+                throw whatTheHell("element can't be null!");
+            }
+            switch (index) {
+                case 0:
+                    client_shares = cast(element, Vector.class);
+                    break;
+                default:
+                    throw whatTheHell("incorrect index %d!", index);
+            }
+        }
     }
     
     public static class ServerHelloImpl extends KeyShareImpl implements ServerHello {
 
-        private final KeyShareEntry server_share;
+        private KeyShareEntry server_share;
 
         ServerHelloImpl(KeyShareEntry server_share) {
             this.server_share = server_share;
@@ -103,12 +141,46 @@ public abstract class KeyShareImpl implements KeyShare {
         public int hashCode() {
             return Objects.hash(server_share);
         }
+
+        @Override
+        public boolean composite() {
+            return true;
+        }
+
+        @Override
+        public int total() {
+            return 1;
+        }
+
+        @Override
+        public Struct element(int index) {
+            switch (index) {
+                case 0:
+                    return server_share;
+                default:
+                    throw whatTheHell("incorrect index %d!", index);
+            }
+        }
+
+        @Override
+        public void element(int index, Struct element) {
+            if (element == null) {
+                throw whatTheHell("element can't be null!");
+            }
+            switch (index) {
+                case 0:
+                    server_share = cast(element, KeyShareEntry.class);
+                    break;
+                default:
+                    throw whatTheHell("incorrect index %d!", index);
+            }
+        }
     }
     
     public static class HelloRetryRequestImpl extends KeyShareImpl 
             implements HelloRetryRequest {
 
-        private final NamedGroup selected_group;
+        private NamedGroup selected_group;
 
         HelloRetryRequestImpl(NamedGroup selected_group) {
             this.selected_group = selected_group;
@@ -149,6 +221,40 @@ public abstract class KeyShareImpl implements KeyShare {
         @Override
         public int hashCode() {
             return Objects.hash(selected_group);
+        }
+
+        @Override
+        public boolean composite() {
+            return true;
+        }
+
+        @Override
+        public int total() {
+            return 1;
+        }
+
+        @Override
+        public Struct element(int index) {
+            switch (index) {
+                case 0:
+                    return selected_group;
+                default:
+                    throw whatTheHell("incorrect index %d!", index);
+            }
+        }
+
+        @Override
+        public void element(int index, Struct element) {
+            if (element == null) {
+                throw whatTheHell("element can't be null!");
+            }
+            switch (index) {
+                case 0:
+                    selected_group = cast(element, NamedGroup.class);
+                    break;
+                default:
+                    throw whatTheHell("incorrect index %d!", index);
+            }
         }
     }
 }
