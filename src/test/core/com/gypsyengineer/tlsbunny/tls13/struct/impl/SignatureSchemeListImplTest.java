d6450c214d717d805f29b53998839d1ebf0fca8e
Artem Smotrakov <artem.smotrakov@gmail.com>
2019-02-24 08:43:27 +0100

    Added NamedGroupListImplTest and SignatureSchemeListImplTest

--- src/test/core/com/gypsyengineer/tlsbunny/tls13/struct/impl/SignatureSchemeListImplTest.java
+++ src/test/core/com/gypsyengineer/tlsbunny/tls13/struct/impl/SignatureSchemeListImplTest.java
@@ -1,9 +1,7 @@
 package com.gypsyengineer.tlsbunny.tls13.struct.impl;
 
 import com.gypsyengineer.tlsbunny.tls.Vector;
-import com.gypsyengineer.tlsbunny.tls13.struct.NamedGroup;
-import com.gypsyengineer.tlsbunny.tls13.struct.NamedGroupList;
-import com.gypsyengineer.tlsbunny.tls13.struct.StructFactory;
+import com.gypsyengineer.tlsbunny.tls13.struct.*;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -14,20 +12,23 @@ import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.*;
 
-public class NamedGroupListImplTest {
+public class SignatureSchemeListImplTest {
 
     @Test
     public void main() throws IOException {
         StructFactory factory = StructFactory.getDefault();
-        NamedGroupList one = factory.createNamedGroupList(NamedGroup.secp256r1);
-        NamedGroupList two = factory.createNamedGroupList(NamedGroup.secp256r1);
+        SignatureSchemeList one = factory.createSignatureSchemeList(
+                SignatureScheme.ecdsa_secp256r1_sha256);
+        SignatureSchemeList two = factory.createSignatureSchemeList(
+                SignatureScheme.ecdsa_secp256r1_sha256);
 
         assertEquals(one, two);
         assertEquals(one.hashCode(), two.hashCode());
         assertEquals(one.encodingLength(), two.encodingLength());
         assertArrayEquals(one.encoding(), two.encoding());
 
-        NamedGroupList three = factory.createNamedGroupList(NamedGroup.ffdhe2048);
+        SignatureSchemeList three = factory.createSignatureSchemeList(
+                SignatureScheme.rsa_pkcs1_sha256);
 
         assertNotEquals(one, three);
         assertNotEquals(one.hashCode(), three.hashCode());
@@ -40,16 +41,17 @@ public class NamedGroupListImplTest {
     @Test
     public void getAndSet() throws Exception {
         StructFactory factory = StructFactory.getDefault();
-        NamedGroupList list = factory.createNamedGroupList(NamedGroup.secp256r1);
+        SignatureSchemeList list = factory.createSignatureSchemeList(
+                SignatureScheme.ecdsa_secp256r1_sha256);
 
         assertTrue(list.composite());
         assertEquals(1, list.total());
 
-        Vector<NamedGroup> groups = Vector.wrap(2, NamedGroup.ffdhe4096);
-        list.element(0, groups);
-        assertSame(groups, list.element(0));
+        Vector<SignatureScheme> schemes = Vector.wrap(2, SignatureScheme.ecdsa_secp384r1_sha384);
+        list.element(0, schemes);
+        assertSame(schemes, list.element(0));
 
         expectWhatTheHell(() -> list.element(7));
-        expectWhatTheHell(() -> list.element(7, groups));
+        expectWhatTheHell(() -> list.element(7, schemes));
     }
 }
