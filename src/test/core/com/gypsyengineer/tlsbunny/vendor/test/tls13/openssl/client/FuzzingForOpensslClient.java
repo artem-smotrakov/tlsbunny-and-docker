b38c1a60459189c9a358c4beedff5913ef0468a9
Artem Smotrakov <artem.smotrakov@gmail.com>
2019-03-02 18:22:41 +0100

    More tests in FuzzingForOpensslClient

--- src/test/core/com/gypsyengineer/tlsbunny/vendor/test/tls13/openssl/client/FuzzingForOpensslClient.java
+++ src/test/core/com/gypsyengineer/tlsbunny/vendor/test/tls13/openssl/client/FuzzingForOpensslClient.java
@@ -1,5 +1,6 @@
 package com.gypsyengineer.tlsbunny.vendor.test.tls13.openssl.client;
 
+import com.gypsyengineer.tlsbunny.tls13.fuzzer.DeepHandshakeFuzzerConfigs;
 import com.gypsyengineer.tlsbunny.tls13.fuzzer.MutatedServer;
 import com.gypsyengineer.tlsbunny.utils.Config;
 import com.gypsyengineer.tlsbunny.utils.SystemPropertiesConfig;
@@ -7,7 +8,7 @@ import com.gypsyengineer.tlsbunny.vendor.test.tls13.VendorTest;
 import com.gypsyengineer.tlsbunny.vendor.test.tls13.openssl.OpensslClient;
 import org.junit.Test;
 
-import static com.gypsyengineer.tlsbunny.tls13.fuzzer.MutatedConfigs.ccsConfigs;
+import static com.gypsyengineer.tlsbunny.tls13.fuzzer.MutatedConfigs.*;
 import static com.gypsyengineer.tlsbunny.tls13.server.HttpsServer.httpsServer;
 
 public class FuzzingForOpensslClient {
@@ -20,6 +21,60 @@ public class FuzzingForOpensslClient {
                 .label("mutated_server_ccs")
                 .set(new OpensslClient())
                 .set(MutatedServer.from(httpsServer(), ccsConfigs(mainConfig)))
+                .run();
+    }
+
+    @Test
+    public void tlsPlaintext() throws Exception {
+        new VendorTest()
+                .label("mutated_server_tls_plaintext")
+                .set(new OpensslClient())
+                .set(MutatedServer.from(httpsServer(), tlsPlaintextConfigs(mainConfig)))
+                .run();
+    }
+
+    @Test
+    public void handshake() throws Exception {
+        new VendorTest()
+                .label("mutated_server_handshake")
+                .set(new OpensslClient())
+                .set(MutatedServer.from(httpsServer(), handshakeConfigs(mainConfig)))
+                .run();
+    }
+
+    @Test
+    public void serverHello() throws Exception {
+        new VendorTest()
+                .label("mutated_server_server_hello")
+                .set(new OpensslClient())
+                .set(MutatedServer.from(httpsServer(), serverHelloConfigs(mainConfig)))
+                .run();
+    }
+
+    @Test
+    public void finished() throws Exception {
+        new VendorTest()
+                .label("mutated_server_finished")
+                .set(new OpensslClient())
+                .set(MutatedServer.from(httpsServer(), finishedConfigs(mainConfig)))
+                .run();
+    }
+
+    @Test
+    public void extensionVector() throws Exception {
+        new VendorTest()
+                .label("mutated_server_extension_vector")
+                .set(new OpensslClient())
+                .set(MutatedServer.from(httpsServer(), extensionVectorConfigs(mainConfig)))
+                .run();
+    }
+
+    @Test
+    public void deepHandshakeFuzzer() throws Exception {
+        new VendorTest()
+                .label("mutated_server_deep_handshake_fuzzer")
+                .set(new OpensslClient())
+                .set(MutatedServer.from(httpsServer(), DeepHandshakeFuzzerConfigs.noClientAuth(mainConfig)))
                 .run();
     }
 }
