d0b5fd4b1d31a0676fd8bfa4a42e61a3d697cf6f
Artem Smotrakov <artem.smotrakov@gmail.com>
2019-04-03 22:07:02 +0200

    Fixed Output.add(Output) method

--- src/main/core/com/gypsyengineer/tlsbunny/output/OutputWrapper.java
+++ src/main/core/com/gypsyengineer/tlsbunny/output/OutputWrapper.java
@@ -93,7 +93,7 @@ public abstract class OutputWrapper implements Output {
 
     @Override
     public void add(Output output) {
-        output.add(output);
+        this.output.add(output);
     }
 
     @Override
