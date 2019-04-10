0fd56cdb860b6457be53876e4c35cf4eed6294c6
Artem Smotrakov <artem.smotrakov@gmail.com>
2019-04-10 21:37:47 +0200

    Simplify build_and_test.sh

--- scripts/build_and_test.sh
+++ scripts/build_and_test.sh
@@ -2,24 +2,11 @@
 
 git fetch --unshallow --quiet
 
-./gradlew clean
-
 ./gradlew \
     --info \
-    -Ptlsbunny.test.exclude=com/gypsyengineer/tlsbunny/vendor/test/tls13/** \
     -Dtlsbunny.output.standard.level=achtung \
     -Dtlsbunny.sync.output=limited \
     -Dtlsbunny.output.enable.highlighting=false \
-        test
-
-./gradlew \
-    --info \
-    -Ptlsbunny.test.include=com/gypsyengineer/tlsbunny/vendor/test/tls13/** \
-    -Dtlsbunny.output.standard.level=achtung \
-    -Dtlsbunny.sync.output=limited \
-    -Dtlsbunny.output.enable.highlighting=false \
-        test
-
-./gradlew jacocoTestReport
+        clean test jacocoTestReport
 
 sonar-scanner
