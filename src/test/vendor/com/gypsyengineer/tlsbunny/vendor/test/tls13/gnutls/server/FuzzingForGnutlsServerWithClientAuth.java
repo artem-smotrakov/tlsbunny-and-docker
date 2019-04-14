36c6720cbf6a07d7dd063bba516073ffe8e3416f
Artem Smotrakov <artem.smotrakov@gmail.com>
2019-04-14 14:27:56 +0200

    Added FuzzingForGnutlsServerWithClientAuth

---
+++ src/test/vendor/com/gypsyengineer/tlsbunny/vendor/test/tls13/gnutls/server/FuzzingForGnutlsServerWithClientAuth.java
@@ -0,0 +1,18 @@
+package com.gypsyengineer.tlsbunny.vendor.test.tls13.gnutls.server;
+
+import com.gypsyengineer.tlsbunny.vendor.test.tls13.Utils;
+import com.gypsyengineer.tlsbunny.vendor.test.tls13.common.server.FuzzingForServerWithClientAuth;
+import org.junit.BeforeClass;
+
+import static com.gypsyengineer.tlsbunny.vendor.test.tls13.gnutls.server.GnutlsServer.gnutlsServer;
+
+public class FuzzingForGnutlsServerWithClientAuth extends FuzzingForServerWithClientAuth {
+
+    @BeforeClass
+    public static void setUp() throws Exception {
+        server = gnutlsServer().clientAuth();
+        server.start();
+        Utils.waitStart(server);
+    }
+
+}
