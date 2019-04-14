7e3427ba8f0cc7f8f51d40391a70ba7eb2202ffc
Artem Smotrakov <artem.smotrakov@gmail.com>
2019-04-14 10:21:56 +0200

    Better GnutlsServer

--- src/main/gnutls/start.sh
+++ src/main/gnutls/start.sh
@@ -5,6 +5,14 @@ options=${options:-"--port=50101 --debug=9999 --http \
                         --x509keyfile certs/server_key.pem \
                         --x509certfile certs/server_cert.pem \
                         --disable-client-cert \
-                        --priority="NORMAL:-VERS-ALL:+VERS-TLS1.3:+VERS-TLS1.2"}
+                        --priority=NORMAL:-VERS-ALL:+VERS-TLS1.3:+VERS-TLS1.2"}
 
-gnutls-serv ${options}
+# looks like gnutls server can exit if a wrong message was received
+# so run it in a loop
+
+touch stop.file
+echo "note: remove $(pwd)/stop.file to stop the server loop"
+while [ -f stop.file ];
+do
+    gnutls-serv ${options} 2>&1
+done
--- src/main/vendor/com/gypsyengineer/tlsbunny/vendor/test/tls13/gnutls/server/GnutlsServer.java
+++ src/main/vendor/com/gypsyengineer/tlsbunny/vendor/test/tls13/gnutls/server/GnutlsServer.java
@@ -32,16 +32,17 @@ public class GnutlsServer extends BaseDockerServer implements Server {
     }
 
     private GnutlsServer() {
-        super(new OutputListenerImpl("ACCEPT", "tlsbunny: accept"));
+        super(new OutputListenerImpl(
+                "HTTP Server listening on", "server is ready to accept"));
         output.prefix("gnutls-server");
         options.put("--port", String.valueOf(defaultPort));
-        options.put("--debug", "9999");
         options.put("--http", no_arg);
         options.put("--x509cafile", "certs/root_cert.pem");
         options.put("--x509keyfile", "certs/server_key.pem");
         options.put("--x509certfile", "certs/server_cert.pem");
         options.put("--disable-client-cert", no_arg);
         options.put("--priority", "NORMAL:-VERS-ALL:+VERS-TLS1.3:+VERS-TLS1.2");
+        dockerEnv.put("ASAN_OPTIONS", "detect_leaks=0");
     }
 
     @Override
@@ -135,7 +136,7 @@ public class GnutlsServer extends BaseDockerServer implements Server {
                     containerName,
                     "bash",
                     "-c",
-                    "pidof gnutls-serv | xargs kill -SIGINT"
+                    "rm /var/src/tlsbunny/stop.file; pidof gnutls-serv | xargs kill -SIGINT"
             );
 
             synchronized (this) {
