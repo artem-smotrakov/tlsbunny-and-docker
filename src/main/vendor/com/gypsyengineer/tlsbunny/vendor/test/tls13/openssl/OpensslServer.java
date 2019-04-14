3e705642acac2fabb14c506ca6a3a87dca0837e1
Artem Smotrakov <artem.smotrakov@gmail.com>
2019-04-14 12:43:12 +0200

    Moved common code to BaseDockerServer

--- src/main/vendor/com/gypsyengineer/tlsbunny/vendor/test/tls13/openssl/OpensslServer.java
+++ src/main/vendor/com/gypsyengineer/tlsbunny/vendor/test/tls13/openssl/OpensslServer.java
@@ -1,34 +1,15 @@
 package com.gypsyengineer.tlsbunny.vendor.test.tls13.openssl;
 
-import com.gypsyengineer.tlsbunny.output.InputStreamOutput;
-import com.gypsyengineer.tlsbunny.output.Output;
 import com.gypsyengineer.tlsbunny.vendor.test.tls13.BaseDockerServer;
-import com.gypsyengineer.tlsbunny.vendor.test.tls13.Utils;
 import com.gypsyengineer.tlsbunny.tls13.server.Server;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import static com.gypsyengineer.tlsbunny.utils.WhatTheHell.whatTheHell;
 
 public class OpensslServer extends BaseDockerServer implements Server {
 
     private static final int defaultPort = 10101;
 
-    private static final int stop_wait_delay = 1000;
-
     private static final String image = System.getProperty(
             "tlsbunny.openssl.docker.image",
             "tlsbunny_openssl_tls13");
-
-    private static final String host_report_directory = String.format(
-            "%s/openssl_report", System.getProperty("user.dir"));
-
-    private final Map<String, String> options = new HashMap<>();
-    private Status status = Status.not_started;
 
     public static OpensslServer opensslServer() {
         return new OpensslServer();
@@ -90,126 +71,17 @@ public class OpensslServer extends BaseDockerServer implements Server {
     }
 
     @Override
-    public Thread start() {
-        if (running()) {
-            throw whatTheHell("the server has already been started!");
-        }
-
-        createReportDirectory(host_report_directory);
-
-        Thread thread = new Thread(this);
-        thread.start();
-
-        return thread;
+    protected int dockerPort() {
+        return defaultPort;
     }
 
     @Override
-    public void run() {
-        if (status() != Status.not_started) {
-            throw whatTheHell("server can't be started twice!");
-        }
-
-        List<String> command = new ArrayList<>();
-        command.add("docker");
-        command.add("run");
-        command.add("-p");
-        command.add(String.format("%d:%d", defaultPort, defaultPort));
-        command.add("-v");
-        command.add(String.format("%s:%s",
-                host_report_directory, container_report_directory));
-
-        StringBuilder sb = new StringBuilder();
-        for (Map.Entry<String, String> entry : options.entrySet()) {
-            sb.append(String.format("%s %s ", entry.getKey(), entry.getValue()));
-        }
-        if (dockerEnv.containsKey("options")) {
-            output.achtung("overwrite environment variable 'options', previous value: %s",
-                    dockerEnv.get("options"));
-        }
-        dockerEnv.put("options", sb.toString());
-
-        for (Map.Entry entry : dockerEnv.entrySet()) {
-            command.add("-e");
-            command.add(String.format("%s=%s", entry.getKey(), entry.getValue()));
-        }
-
-        command.add("--name");
-        command.add(containerName);
-        command.add(image);
-
-        synchronized (this) {
-            status = Server.Status.ready;
-        }
-        try {
-            Process process = Utils.exec(output, command);
-            output.set(process.getInputStream());
-            int code = process.waitFor();
-            if (code != 0) {
-                output.achtung("the server exited with a non-zero exit code (%d)", code);
-
-                synchronized (this) {
-                    failed = true;
-                }
-            }
-        } catch (InterruptedException | IOException e) {
-            output.achtung("unexpected exception occurred", e);
-
-            synchronized (this) {
-                failed = true;
-            }
-        } finally {
-            synchronized (this) {
-                status = Server.Status.done;
-            }
-        }
+    protected String dockerImage() {
+        return image;
     }
 
     @Override
-    public Status status() {
-        synchronized (this) {
-            return status;
-        }
-    }
-
-    @Override
-    public OpensslServer stop() {
-        try {
-            List<String> command = List.of(
-                    "docker",
-                    "exec",
-                    containerName,
-                    "bash",
-                    "-c",
-                    "pidof openssl | xargs kill -SIGINT"
-            );
-
-            synchronized (this) {
-                status = Server.Status.ready;
-            }
-            try {
-                Process process = Utils.exec(output, command);
-                InputStreamOutput commandOutput = Output.create(process.getInputStream());
-                int code = process.waitFor();
-                output.add(commandOutput);
-
-                if (code != 0) {
-                    output.achtung("could not stop the server (exit code %d)", code);
-                    failed = true;
-                }
-
-                while (containerRunning()) {
-                    Utils.sleep(stop_wait_delay);
-                }
-            } finally {
-                synchronized (this) {
-                    status = Server.Status.done;
-                }
-            }
-        } catch (InterruptedException | IOException e) {
-            output.achtung("unexpected exception occurred", e);
-            failed = true;
-        }
-
-        return this;
+    protected String killCommand() {
+        return "pidof openssl | xargs kill -SIGINT";
     }
 }
--- src/main/vendor/com/gypsyengineer/tlsbunny/vendor/test/tls13/wolfssl/WolfsslServer.java
+++ src/main/vendor/com/gypsyengineer/tlsbunny/vendor/test/tls13/wolfssl/WolfsslServer.java
@@ -3,13 +3,6 @@ package com.gypsyengineer.tlsbunny.vendor.test.tls13.wolfssl;
 import com.gypsyengineer.tlsbunny.tls13.server.Server;
 import com.gypsyengineer.tlsbunny.vendor.test.tls13.AddressSanitizerWatcherOutput;
 import com.gypsyengineer.tlsbunny.vendor.test.tls13.BaseDockerServer;
-import com.gypsyengineer.tlsbunny.vendor.test.tls13.Utils;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 public class WolfsslServer extends BaseDockerServer implements Server {
 
@@ -21,9 +14,6 @@ public class WolfsslServer extends BaseDockerServer implements Server {
     private static final String image = System.getProperty(
             "tlsbunny.wolfssl.docker.image",
             "tlsbunny_wolfssl_tls13");
-
-    private final Map<String, String> options = new HashMap<>();
-    private Status status = Status.not_started;
 
     public static WolfsslServer wolfsslServer() {
         return new WolfsslServer();
@@ -57,88 +47,17 @@ public class WolfsslServer extends BaseDockerServer implements Server {
     }
 
     @Override
-    public void run() {
-        List<String> command = new ArrayList<>();
-        command.add("docker");
-        command.add("run");
-        command.add("-p");
-        command.add(String.format("%d:%d", defaultPort, defaultPort));
-
-        StringBuilder sb = new StringBuilder();
-        for (Map.Entry<String, String> entry : options.entrySet()) {
-            sb.append(String.format("%s %s ", entry.getKey(), entry.getValue()));
-        }
-        if (dockerEnv.containsKey("options")) {
-            output.achtung("overwrite environment variable 'options', previous value: %s",
-                    dockerEnv.get("options"));
-        }
-        dockerEnv.put("options", sb.toString());
-
-        for (Map.Entry entry : dockerEnv.entrySet()) {
-            command.add("-e");
-            command.add(String.format("%s=%s", entry.getKey(), entry.getValue()));
-        }
-
-        command.add("--name");
-        command.add(containerName);
-        command.add(image);
-
-        synchronized (this) {
-            status = Status.ready;
-        }
-        try {
-            Process process = Utils.exec(output, command);
-            output.set(process.getInputStream());
-            int code = process.waitFor();
-            if (code != 0) {
-                output.achtung("the server exited with a non-zero exit code (%d)", code);
-
-                synchronized (this) {
-                    failed = true;
-                }
-            }
-        } catch (InterruptedException | IOException e) {
-            output.achtung("unexpected exception occurred", e);
-
-            synchronized (this) {
-                failed = true;
-            }
-        } finally {
-            synchronized (this) {
-                status = Status.done;
-            }
-        }
+    protected int dockerPort() {
+        return defaultPort;
     }
 
     @Override
-    public Status status() {
-        synchronized (this) {
-            return status;
-        }
+    protected String dockerImage() {
+        return image;
     }
 
     @Override
-    public WolfsslServer stop() {
-        try {
-            List<String> command = List.of(
-                    "docker",
-                    "exec",
-                    containerName,
-                    "bash",
-                    "-c",
-                    "rm /var/src/wolfssl/stop.file; pidof lt-server | xargs kill -9"
-            );
-
-            int code = Utils.waitProcessFinish(output, command);
-            if (code != 0) {
-                output.achtung("could not stop the server (exit code %d)", code);
-                failed = true;
-            }
-        } catch (InterruptedException | IOException e) {
-            output.achtung("unexpected exception occurred", e);
-            failed = true;
-        }
-
-        return this;
+    protected String killCommand() {
+        return "rm /var/src/wolfssl/stop.file; pidof lt-server | xargs kill -9";
     }
 }
