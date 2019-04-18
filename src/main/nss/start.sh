c6fc7d8b9aac3f8639b5eeabd3356719d966bb98
Artem Smotrakov <artem.smotrakov@gmail.com>
2019-04-18 23:01:23 +0200

    Configurable options in src/main/nss/start.sh

--- src/main/nss/start.sh
+++ src/main/nss/start.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-selfserv -v -d sql:./nssdb -p 60101 -V tls1.3:tls1.3 -H 1 -n localhost
+options=${options:-"-v -d sql:./nssdb -p 60101 -V tls1.3:tls1.3 -H 1 -n localhost"}
+
+selfserv ${options}
